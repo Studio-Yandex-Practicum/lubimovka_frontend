76505148b1c1142748c50c40abbbf5c8d936cd9f
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-09-13 08:10:50 +0300

    fix: исправление ошибок ESLint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-const Home = () => (
+import { NextPage } from 'next';
+
+const Home: NextPage = () => (
   <h1>
     Hello, world!
   </h1>
--- src/shared/breakpoints.js
+++ src/shared/breakpoints.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-undef
 module.exports =  {
   'mobile-landscape': '576px',
   'tablet-portrait': '768px',
   'tablet-landscape': '992px',
   laptop: '1200px',
   desktop: '1400px',
-}
+};
