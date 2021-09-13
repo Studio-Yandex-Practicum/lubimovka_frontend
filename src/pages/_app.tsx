7ee6a7d5ab89edcee3ce850f03aa22bbd94c174e
PyotrGrogorchenko <pepegrigorchenko@yandex.ru>
2021-09-13 14:20:31 +0300

    Revert "Первоначальная настройка проекта"

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from 'next/app';
-
-import 'shared/styles/palette.css';
-
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
-}
