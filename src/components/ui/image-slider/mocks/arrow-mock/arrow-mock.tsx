5fdf10465d7f3718e5fa8aacf19550612d399862
Ilya Borodulin <borodulinid@gmail.com>
2021-09-15 17:41:16 +0300

    feat: мок-компоненты для слайдера

---
+++ src/components/ui/image-slider/mocks/arrow-mock/arrow-mock.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+
+interface ArrowMockProps {
+  className?: string;
+  direction?: 'right' | 'left';
+  onClick?: () => void
+}
+
+export const ArrowMock: FC<ArrowMockProps> = (props) => {
+  const { className, onClick } = props;
+
+  return (
+    <svg
+      className={className}
+      width="50"
+      height="50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+    >
+      <circle cx="25" cy="25" r="25" fill="#242424"/>
+    </svg>
+  );
+};
