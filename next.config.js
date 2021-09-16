93e1a1371691e68469767f51b1e89c9f70019058
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-09-16 10:01:57 +0300

    build: настройка импорта svg

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: {
+                removeViewBox: false,
+              }
+            }
+          }
+        }
+      ],
+    });
+
+    return config;
+  },
+};
