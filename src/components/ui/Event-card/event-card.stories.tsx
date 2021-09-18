8f63edab0712df405210b2fbcb0333ac5ff13633
Ron Winchester <RonWinchester@yandex.ru>
2021-09-18 17:23:15 +0300

    feat: storybook

---
+++ src/components/ui/Event-card/event-card.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { EventСard } from './event-card';
+
+export default {
+  title: 'Example/EventСard',
+  component: EventСard,
+} as ComponentMeta<typeof EventСard>;
+
+const Template: ComponentStory<typeof EventСard> = (args) => <EventСard {...args} />;
+
+export const Event_Сard = Template.bind({});
+Event_Сard.args = {
+  img : 'https://images.unsplash.com/photo-1621636723658-a062df4cbb2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+  time : '13:00',
+  location : 'Площадка «8/3»',
+  title :'Что я узнал',
+  description:'Гости расскажут о своём творческом и организационном опыте.',
+  playwright :'Ольга Казакова',
+  direct :'Катя Ганюшина'
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,7 @@
 import { NextPage } from 'next';
-import { EventСard } from 'components/ui/Event-card';
-const image = 'https://images.unsplash.com/photo-1621636723658-a062df4cbb2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80';
-const time = '13:00';
-const location = 'Площадка «8/3»';
-const title = 'Что я узнал';  
-const description = 'Гости расскажут о своём творческом и организационном опыте.';
-const playwright = 'Ольга Казакова';
-const direct = 'Катя Ганюшина';
+
 const Home: NextPage = () => (
-  <>
-    <h1>Hello, world!</h1>
-    <EventСard img={image} time={time} location={location} title={title} description={description} playwright={playwright} direct={direct}></EventСard>
-    <EventСard /* img={image} */ time={time} location={location} title={title} description={description} playwright={playwright} direct={direct}></EventСard>
-    <EventСard img={image} time={time} location={location} title={title} description={description} playwright={playwright} direct={direct}></EventСard>
-  </>
+  <h1>Hello, world!</h1>
 );
 
 export default Home;
