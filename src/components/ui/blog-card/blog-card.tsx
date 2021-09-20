f3244723bf0345c13975bc9420875532d432e809
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-20 23:23:11 +0300

    feat: добавить обработчик клика

--- src/components/ui/blog-card/blog-card.tsx
+++ src/components/ui/blog-card/blog-card.tsx
@@ -6,13 +6,18 @@ interface BlogCardProps {
   author: string;
   heading: string;
   description: string;
+  onClick?: (evt) => void;
 }
 
 export const BlogCard: FC<BlogCardProps> = (props) => {
-  const { image, author, heading, description } = props;
+  const { image, author, heading, description, onClick } = props;
+
+  function handleCardClick(evt) {
+    onClick(evt);
+  }
 
   return (
-    <li className={styles.card}>
+    <li className={styles.card} onClick={handleCardClick}>
       <div className={styles.img_container}>
         <img className={styles.img} src={image} alt="изображение пьесы" />
       </div>
