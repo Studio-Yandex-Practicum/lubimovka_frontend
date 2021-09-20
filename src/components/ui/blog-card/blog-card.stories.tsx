e8f34ce467105182dea531dc48da738bc3042955
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-20 23:00:55 +0300

    feat: добавить карточку в сетке storybook

--- src/components/ui/blog-card/blog-card.stories.tsx
+++ src/components/ui/blog-card/blog-card.stories.tsx
@@ -17,3 +17,28 @@ BlogCard_default.args = {
   heading: '«Человек является источником жеста, смысла, правды»',
   description: 'МК «Документальность в современном театре и современном искусстве»',
 };
+
+const CardBlockStyles = {
+  'margin': '0 auto',
+  'padding': '0',
+  'listStyle': 'none',
+  'maxWidth': '1260px',
+  'display': 'grid',
+  'gridAutoFlow': 'dense',
+  'gridTemplateColumns': 'repeat(auto-fill, 270px)',
+  'justifyContent': 'center',
+  'gap': '47px 60px',
+  'position': 'relative'
+};
+
+export const BlogCard_in_grid_first = (args) => (
+  <ui style={CardBlockStyles}>
+    <BlogCard {...args} />
+  </ui>
+);
+BlogCard_in_grid_first.args = {
+  image: 'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_iqshami78.jpg',
+  author: 'Лара Бессмертная',
+  heading: '«Человек является источником жеста, смысла, правды»',
+  description: 'МК «Документальность в современном театре и современном искусстве»',
+};
