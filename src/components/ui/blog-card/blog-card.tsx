19ef2337bbb9a0bdd3439a70b3f460873f28d6ad
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-20 22:31:10 +0300

    feat: создать структуру компонента

---
+++ src/components/ui/blog-card/blog-card.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import styles from './blog-card.module.css';
+
+interface BlogCardProps {
+  image: string;
+  author: string;
+  heading: string;
+  description: string;
+}
+
+export const BlogCard: FC<BlogCardProps> = (props) => {
+  const { image, author, heading, description } = props;
+
+  return (
+    <li>
+      <div>
+        <img src={image} alt="изображение пьесы" />
+      </div>
+      <div>
+        <p>{author}</p>
+        <h6>{heading}</h6>
+        <p>{description}</p>
+      </div>
+    </li>
+  );
+};
+
