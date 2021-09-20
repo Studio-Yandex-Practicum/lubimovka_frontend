4dcd29f108154da02f5898e45430e1050fde543d
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-20 23:21:01 +0300

    feat: добавить вид карточек в сетке storybook

--- src/components/ui/blog-card/blog-card.stories.tsx
+++ src/components/ui/blog-card/blog-card.stories.tsx
@@ -42,3 +42,35 @@ BlogCard_in_grid_first.args = {
   heading: '«Человек является источником жеста, смысла, правды»',
   description: 'МК «Документальность в современном театре и современном искусстве»',
 };
+
+export const BlogCard_in_grid_some = (args) => (
+  <ui style={CardBlockStyles}>
+    <BlogCard {...args} />
+    <BlogCard
+      {...args}
+      image={'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_iqshami78.jpg'}
+    />
+    <BlogCard
+      {...args}
+      image={'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_6.jpg'}
+    />
+    <BlogCard
+      {...args}
+      image={'https://i.pinimg.com/736x/8b/7b/ac/8b7bac354c8982477a701af6ca9e9128.jpg'}
+    />
+    <BlogCard
+      {...args}
+      image={'http://kudago.com/media/images/event/43/7a/437a0e0f05a59d217408ebe4380f72a7.jpg'}
+    />
+    <BlogCard
+      {...args}
+      image={'https://i1.wp.com/altereos.ru/wp-content/uploads/2019/05/Neokonchennaya-pesa-dlya-mehanicheskogo-pianino-1977.mkv-00003.jpg?ssl=1'}
+    />
+  </ui>
+);
+BlogCard_in_grid_some.args = {
+  image: 'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_iqshami78.jpg',
+  author: 'Лара Бессмертная',
+  heading: '«Человек является источником жеста, смысла, правды»',
+  description: 'МК «Документальность в современном театре и современном искусстве»',
+};
