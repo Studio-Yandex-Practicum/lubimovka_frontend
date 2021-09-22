0bf95541078d5241be0737bac0d67cef8ef311e2
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-22 18:55:05 +0300

    fix: исправить ошибки storybook (переписан через декораторы)

--- src/components/ui/blog-card/blog-card.stories.tsx
+++ src/components/ui/blog-card/blog-card.stories.tsx
@@ -1,79 +1,72 @@
 import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { BlogCard } from './blog-card';
 
 export default {
   component: BlogCard,
   title: 'Blog Card',
-} as Meta;
+  argTypes: {
+    onClick: { action: 'clicked' },
+  },
+} as ComponentMeta<typeof BlogCard>;
 
-const Template: Story<typeof BlogCard> = (args) => <BlogCard {...args} />;
+const Template: ComponentStory<typeof BlogCard> = (args) => <BlogCard {...args} />;
 
-export const BlogCard_default = Template.bind({});
-BlogCard_default.args = {
+export const only_first_BlogCard = Template.bind({});
+only_first_BlogCard.args = {
   image: 'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_iqshami78.jpg',
   author: 'Лара Бессмертная',
   heading: '«Человек является источником жеста, смысла, правды»',
   description: 'МК «Документальность в современном театре и современном искусстве»',
-  argTypes: { onClick: { action: 'clicked' } },
 };
-
-const CardBlockStyles = {
-  'margin': '0 auto',
-  'padding': '0',
-  'listStyle': 'none',
-  'maxWidth': '1260px',
-  'display': 'grid',
-  'gridAutoFlow': 'dense',
-  'gridTemplateColumns': 'repeat(auto-fill, 270px)',
-  'justifyContent': 'center',
-  'gap': '47px 60px',
-  'position': 'relative'
-};
-
-export const BlogCard_in_grid_first = (args) => (
-  <ui style={CardBlockStyles}>
-    <BlogCard {...args} />
-  </ui>
-);
-BlogCard_in_grid_first.args = {
+only_first_BlogCard.decorators = [
+  (Story) => (
+    <div style={{
+      margin: '0 auto',
+      padding: '0',
+      listStyle: 'none',
+      maxWidth: '1260px',
+      display: 'grid',
+      gridAutoFlow: 'dense',
+      gridTemplateColumns: 'repeat(auto-fill, 270px)',
+      justifyContent: 'center',
+      gap: '47px 60px',
+      position: 'relative'
+    }}>
+      <Story />
+    </div >
+  )
+];
+export const some_BlogCard = Template.bind({});
+some_BlogCard.args = {
   image: 'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_iqshami78.jpg',
   author: 'Лара Бессмертная',
   heading: '«Человек является источником жеста, смысла, правды»',
   description: 'МК «Документальность в современном театре и современном искусстве»',
-  argTypes: { onClick: { action: 'clicked' } },
 };
-
-export const BlogCard_in_grid_some = (args) => (
-  <ui style={CardBlockStyles}>
-    <BlogCard {...args} />
-    <BlogCard
-      {...args}
-      image={'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_iqshami78.jpg'}
-    />
-    <BlogCard
-      {...args}
-      image={'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_6.jpg'}
-    />
-    <BlogCard
-      {...args}
-      image={'https://i.pinimg.com/736x/8b/7b/ac/8b7bac354c8982477a701af6ca9e9128.jpg'}
-    />
-    <BlogCard
-      {...args}
-      image={'http://kudago.com/media/images/event/43/7a/437a0e0f05a59d217408ebe4380f72a7.jpg'}
-    />
-    <BlogCard
-      {...args}
-      image={'https://i1.wp.com/altereos.ru/wp-content/uploads/2019/05/Neokonchennaya-pesa-dlya-mehanicheskogo-pianino-1977.mkv-00003.jpg?ssl=1'}
-    />
-  </ui>
-);
-BlogCard_in_grid_some.args = {
-  image: 'https://img51994.domkino.tv/img/2018-10-29/fmt_114_24_iqshami78.jpg',
-  author: 'Лара Бессмертная',
-  heading: '«Человек является источником жеста, смысла, правды»',
-  description: 'МК «Документальность в современном театре и современном искусстве»',
-  argTypes: { onClick: { action: 'clicked' } },
-};
+some_BlogCard.decorators = [
+  (Story) => (
+    <div style={{
+      margin: '0 auto',
+      padding: '0',
+      listStyle: 'none',
+      maxWidth: '1260px',
+      display: 'grid',
+      gridAutoFlow: 'dense',
+      gridTemplateColumns: 'repeat(auto-fill, 270px)',
+      justifyContent: 'center',
+      gap: '47px 60px',
+      position: 'relative'
+    }}>
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+      <Story />
+    </div >
+  )
+];
