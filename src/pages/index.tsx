4da6821618d9ce56e21a34a2f911ee9100d653bd
Ron Winchester <RonWinchester@yandex.ru>
2021-09-22 14:57:51 +0300

    fix: использование констант для цвета

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import { NextPage } from 'next';
+import { EventСard } from 'components/ui/event-card';
 
 const Home: NextPage = () => (
-  <h1>
+  <>
+    <h1>
     Hello, world!
-  </h1>
+    </h1>
+    <EventСard   img = 'https://images.unsplash.com/photo-1621636723658-a062df4cbb2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80'
+      time = '13:00'
+      location = 'Площадка «8/3»'
+      title ='Что я узнал'
+      description='Гости расскажут о своём творческом и организационном опыте.'
+      playwright ='Ольга Казакова'
+      direct ='Катя Ганюшина'></EventСard>
+  </>
 );
 
 export default Home;
