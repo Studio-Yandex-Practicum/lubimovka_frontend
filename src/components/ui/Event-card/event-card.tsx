e8c151cc402954d1b6e27623c28ac59fd042c1a2
Ron Winchester <RonWinchester@yandex.ru>
2021-09-22 13:29:05 +0300

    fix: удалил лишний фрагмент

--- src/components/ui/Event-card/event-card.tsx
+++ src/components/ui/Event-card/event-card.tsx
@@ -13,25 +13,29 @@ export const EventСard: FC<ICardProps> = (props) => {
   const { time, location, title, description, playwright, direct } = props;
 
   return (
-    <>
-      <div className={styles.event}>
-        <div className={`${styles.content} ${!props.img ? styles.contentNoImage : ''}`}>
-          {props.img && <img src={props.img} alt={title} className={styles.image} />}
-          <div className={styles.data}>
-            <p className={styles.time}>{time}</p>
-            <p className={styles.location}>{location}</p>
-          </div>
-          <div className={styles.data}>
-            <h2 className={styles.title}>{title}</h2>
-            <p className={styles.description}>{description}</p>
-          </div>
-          <ul className={styles.creators}>
-            <li>Драматург: {playwright}</li>
-            <li>Режиссер: {direct}</li>
-          </ul>
-          <button className={styles.button}></button>
+    <div className={styles.event}>
+      <div
+        className={`${styles.content} ${
+          !props.img ? styles.contentNoImage : ''
+        }`}
+      >
+        {props.img && (
+          <img src={props.img} alt={title} className={styles.image} />
+        )}
+        <div className={styles.data}>
+          <p className={styles.time}>{time}</p>
+          <p className={styles.location}>{location}</p>
         </div>
+        <div className={styles.data}>
+          <h2 className={styles.title}>{title}</h2>
+          <p className={styles.description}>{description}</p>
+        </div>
+        <ul className={styles.creators}>
+          <li>Драматург: {playwright}</li>
+          <li>Режиссер: {direct}</li>
+        </ul>
+        <button className={styles.button}></button>
       </div>
-    </>
+    </div>
   );
 };
