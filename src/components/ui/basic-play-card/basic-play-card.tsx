0d731e9ccae9addc12a15e60e43c90e8913b9b71
Anya Ch <annnacha@yandex.ru>
2021-09-25 02:11:54 +0300

    fix: базовая карточка пьесы : фиксы по переполнению контента в карточке

--- src/components/ui/basic-play-card/basic-play-card.tsx
+++ src/components/ui/basic-play-card/basic-play-card.tsx
@@ -36,29 +36,29 @@ export const BasicPlayCard: FC<IBasicPlayCardProps> = (props) => {
       <div className={cn(styles.container)}>
         <p className={cn(styles.h7, styles.title)}>{play.title}</p>
         <div className={cn(styles.buttonContainer)}>
-          <a href={play.linkView} className={cn(styles.linkReset)}>
+          <Link href={play.linkView}>
             <button className={cn(styles.button)}>
               <p className={cn(styles.buttonName, styles.smalltext)}>CМОТРЕТЬ ЧИТКУ</p>
               <ButtonIconExternal className={cn(styles.buttonIcon)} alt="Иконка кнопки"/>
             </button>
-          </a>
-          <a href={play.linkDownload} className={cn(styles.linkReset)}>
+          </Link>
+          <Link href={play.linkDownload}>
             <button className={cn(styles.button)}>
               <p className={cn(styles.buttonName, styles.smalltext)}>СКАЧАТЬ ПЬЕСУ</p>
               <ButtonIconDownload className={cn(styles.buttonIcon)} alt="Иконка кнопки"/>
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       <div className={cn(styles.info)}>
         <Link href={`/authors/${author.id}`} passHref>
           <div className={cn(styles.author)}>
-            <div className={cn(styles.h7)}>{author.firstName}</div>
-            <div className={cn(styles.h7)}>{author.lastName}</div>
+            <p className={cn(styles.h7, styles.authorName)}>{author.firstName}</p>
+            <p className={cn(styles.h7, styles.authorName)}>{author.lastName}</p>
           </div>
         </Link>
-        <div className={cn(styles.smalltext, styles.city)}>{play.city}</div>
-        <div className={cn(styles.smalltext, styles.year)}>{play.year}</div>
+        <p className={cn(styles.smalltext, styles.city)}>{play.city}</p>
+        <p className={cn(styles.smalltext, styles.year)}>{play.year}</p>
       </div>
     </li>
   );
