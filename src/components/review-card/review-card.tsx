471fea5ab1435ee4e8a115a635b4cc42e442132e
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-09-25 15:17:07 +0300

    feat: добавление карточки отзыва зрителя

---
+++ src/components/review-card/review-card.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import classNames from 'classnames/bind';
+
+import styles from './review-card.module.css';
+
+interface IReviewCardProps {
+  text: string,
+  author: string,
+}
+
+const cx = classNames.bind(styles);
+
+export const ReviewCard: FC<IReviewCardProps> = (props) => {
+  const {
+    text,
+    author,
+  } = props;
+
+  return (
+    <article className={cx('card')}>
+      <p className={cx('text')}>
+        {text}
+      </p>
+      <dl className={cx('info')}>
+        <dt className={cx('hiddenText')}>
+          Автор:
+        </dt>
+        <dd className={cx('author')}>
+          {author}
+        </dd>
+      </dl>
+    </article>
+  );
+};
