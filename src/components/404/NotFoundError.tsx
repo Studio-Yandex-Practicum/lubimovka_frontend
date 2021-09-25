ad72e47c5a10d8b6a1f10887c918444b119e2f5e
ritonpiton <rituhanych@yandex.ru>
2021-09-25 23:06:40 +0300

    feat: добавлена 404 страница

---
+++ src/components/404/NotFoundError.tsx
@@ -0,0 +1,32 @@
+import classes from './NotFoundError.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../../images/logo.svg';
+import lines from '../../images/404lines.svg';
+
+export default function NotFoundError() {
+  return (
+    <section className={classes.error}>
+      <div className={classes.logoContainer}>
+        <Image className={classes.logo} src={'/images/logo.svg'} layout={'fill'}/>
+      </div>
+      <div className={classes.contentContainer}>
+        <h1 className={classes.title}>Ощибка 404</h1>
+        <h2 className={classes.subtitle}>Кажется, такой страницы не существует</h2>
+        <p className={classes.text}>Для этого может быть несколько причин: неправильный адрес, мы ее удалили, ее вообще никогда не существовало или вам это снится.</p>
+        <ul className={classes.list}>
+          <li className={classes.listItem}><Link href={'#'}><a className={classes.link}>&rarr; На главную</a></Link></li>
+          <li className={classes.listItem}><Link href={'#'}><a className={classes.link}>&rarr; Подать пьесу</a></Link></li>
+          <li className={classes.listItem}><Link href={'#'}><a className={classes.link}>&rarr; Поддержать</a></Link></li>
+        </ul>
+      </div>
+      <div className={classes.lines}>
+        <div className={classes.lineContainer}>
+          <Image className={classes.line} src={'/404lines.svg'} layout='fill'/>
+          {/*<Image className={classes.line} src={lines}/>*/}
+        </div>
+      </div>
+    </section>
+
+  );
+}
