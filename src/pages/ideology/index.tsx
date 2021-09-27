dcfd82902eaa6bd2afb66b0fb635d961a3d63135
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-09-27 14:50:38 +0400

    fix: удалить лишние файлы

--- src/pages/ideology/index.tsx
+++ src/pages/ideology/index.tsx
@@ -4,13 +4,11 @@ import TextSection from '../../components/ideology-section';
 
 import textData from './assets/mock-data.json';
 
-import style from './ideology.module.css';
-
 const Ideology: NextPage = () => (
-  <div className={style.page}>
+  <>
     <TextSection isSectionSecond={false} number="1" title="Мы твердо верим, что" data={textData.slice(0, 3)} />
     <TextSection isSectionSecond={true} number="2" title="Мы искренне хотим" data={textData.slice(3)} />
-  </div>
+  </>
 );
 
 export default Ideology;
