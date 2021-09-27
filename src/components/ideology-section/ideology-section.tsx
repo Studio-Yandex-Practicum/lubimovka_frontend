0fcc9b92fa9633b501219a9e27044dd86e20ca20
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-09-27 18:24:18 +0400

    refactor: универсиализировать TextSection и внести нужные правки в mock-data и css-стили

--- src/components/ideology-section/ideology-section.tsx
+++ src/components/ideology-section/ideology-section.tsx
@@ -13,23 +13,26 @@ interface TextItemData {
 }
 
 interface TextSectionProps {
-  isSectionSecond: boolean
-  number: string
-  title: string
-  data: Array<TextItemData>
+  data: {
+    id: number
+    number: string
+    title: string
+    image: string
+    content: Array<TextItemData>
+  }
 }
 
-const TextSection: FC<TextSectionProps> = (props) => {
-  const { isSectionSecond, number, title, data } = props;
+const TextSection: FC<TextSectionProps> = ({ data }) => {
+  const { number, title, image, content } = data;
 
   return (
     <section className={style.section}>
-      <div className={isSectionSecond ? cn(style.container, style.containerSecond) : style.container}>
-        <article className={isSectionSecond ? cn(style.picture, style.pictureAudience) : cn(style.picture, style.pictureActors)} />
-        <div className={isSectionSecond ? style.list : cn(style.list, style.listShiftRight)}>
+      <div className={style.container}>
+        <img src={image} alt="Изображение" className={style.picture} />
+        <div className={style.list}>
           <p className={cn(style.paragraph, style.numberBox)}>{number}</p>
           <h2 className={cn(style.title, style.titleBox)}>{title}</h2>
-          {data.map((el) => (
+          {content.map((el) => (
             <TextItem key={el.id} number={el.number} title={el.title}>
               {el.text.map((item, index) => (
                 <p key={index} className={cn(style.paragraph, style.paragraphText)}>{item}</p>
--- src/pages/ideology/index.tsx
+++ src/pages/ideology/index.tsx
@@ -6,8 +6,9 @@ import textData from './assets/mock-data.json';
 
 const Ideology: NextPage = () => (
   <>
-    <TextSection isSectionSecond={false} number="1" title="Мы твердо верим, что" data={textData.slice(0, 3)} />
-    <TextSection isSectionSecond={true} number="2" title="Мы искренне хотим" data={textData.slice(3)} />
+    {textData.map((el) => (
+      <TextSection key={el.id} data={el} />
+    ))}
   </>
 );
 
