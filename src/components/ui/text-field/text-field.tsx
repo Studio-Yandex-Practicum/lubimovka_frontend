51dbdf6c8b4507cd294ae08c3a65b3473938c736
Tatiana S <tatyana-selyuk@yandex.com>
2021-09-27 11:15:14 +0300

    chore: Вынести text-field как отдельный компонент со своим storybook

--- src/components/ui/text-field/text-field.tsx
+++ src/components/ui/text-field/text-field.tsx
@@ -1,18 +1,20 @@
 import {FC, InputHTMLAttributes} from 'react';
 import cn from 'classnames';
-import styles from './input.module.css';
+import styles from '../input/input.module.css';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   rows?: number;
   cols?: number;
   minLength?: number;
   maxLength?: number;
+  formNoValidate?: boolean;
 }
 
-const TextField: FC<InputProps> = () => {
+const TextField: FC<InputProps> = (props) => {
   return (
     <div className={cn(styles.inputComponent)}>
-      <textarea className={cn(styles.input)}/>
+      <textarea className={cn(styles.input)} />
+      <span className='inputError'>{props.formNoValidate}</span>
     </div>
   );
 };
