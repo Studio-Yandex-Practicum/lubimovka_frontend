467434e478236ac4d523bf5ab320069a4600db1b
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-09-27 10:57:12 +0400

    fix: привязать картинки к контейнеру

--- src/pages/ideology/index.tsx
+++ src/pages/ideology/index.tsx
@@ -8,8 +8,8 @@ import style from './ideology.module.css';
 const Ideology: NextPage = () => (
   <div className={style.page}>
     <section className={style.section}>
-      <article className={cn(style.picture, style.pictureActors)} />
       <div className={style.container}>
+        <article className={cn(style.picture, style.pictureActors)} />
         <div className={cn(style.list, style.listShiftRight)}>
           <p className={cn(style.paragraph, style.numberBox)}>1</p>
           <h2 className={cn(style.title, style.titleBox)}>Мы твердо верим, что</h2>
@@ -31,8 +31,8 @@ const Ideology: NextPage = () => (
     </section>
 
     <section className={cn(style.section, style.sectionSecond)}>
-      <article className={cn(style.picture, style.pictureAudience)} />
       <div className={cn(style.container, style.containerSecond)}>
+        <article className={cn(style.picture, style.pictureAudience)} />
         <div className={style.list}>
           <p className={cn(style.paragraph, style.numberBox)}>2</p>
           <h2 className={cn(style.title, style.titleBox)}>Мы искренне хотим</h2>
