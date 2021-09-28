09eb8fc37d0d0e7fd88d4728b46a9f8b2ff17e6c
Ron Winchester <RonWinchester@yandex.ru>
2021-09-28 23:56:37 +0300

    fix: комментарии в css и правка storybook

--- src/components/ui/Event-card/event-card.stories.tsx
+++ src/components/ui/Event-card/event-card.stories.tsx
@@ -1,29 +1,85 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
 import { EventСard } from './event-card';
 
+const eventCardArgs = [
+  {
+    time:'13:00',
+    location:'Площадка «8/3»',
+    title:'Что я узнал',
+    description:'Гости',
+    playwright:'Ольга',
+    direct:'Катя',
+    image:
+    'https://images.unsplash.com/photo-1621636723658-a062df4cbb2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+  },
+  {
+    time:'19:00',
+    location:'Площадка «8/3»',
+    title:'Что я узнал о творчестве благодаря драматургам',
+    description:'(Не)деликатная пьеса о психоневрологическом интернате',
+    playwright:'Ольга Казакова',
+    direct:'Катя Ганюшина'
+  },
+  {
+    time:'19:00',
+    location:'Площадка «8/3»',
+    title:'Что я узнал о творчестве благодаря драматургам',
+    description:'(Не)деликатная пьеса о психоневрологическом интернате',
+    playwright:'Ольга Казакова',
+    direct:'Катя Ганюшина',
+    image:
+    'https://images.unsplash.com/photo-1621636723658-a062df4cbb2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+  },
+  {
+    time:'19:00',
+    location:'Площадка «8/3»',
+    title:'Что я узнал',
+    description:'(Не)деликатная пьеса о психоневрологическом интернате',
+    playwright:'Ольга Казакова',
+    direct:'Катя Ганюшина'
+  },
+];
+
+const oneCard = eventCardArgs.filter((args,index) => index === 0);
+
 export default {
-  title: 'UI/EventСard',
+  title: 'Components/EventСard',
   component: EventСard,
 } as ComponentMeta<typeof EventСard>;
 
 const Template: ComponentStory<typeof EventСard> = (args) => {
   return (
     <>
-      <EventСard {...args} />
-      <EventСard
-        time="13:00"
-        location="Площадка «8/3»"
-        title="Что я узнал"
-        description="Гости"
-        playwright="Ольга Ка"
-        direct="Катя"
-      />
-      <EventСard {...args} />
+      {eventCardArgs.map((cardArgs, index) => (
+        <EventСard key={index} {...cardArgs}/>
+      ))}
     </>
   );
 };
+const TemplateOneCard: ComponentStory<typeof EventСard> = (args) => {
+  return (
+    <>
+      {oneCard.map((cardArgs, index) => (
+        <EventСard key={index} {...cardArgs}/>
+      ))}
+    </>
+  );
+};
+
+export const Event_One_Сard = TemplateOneCard.bind({});
+Event_One_Сard.args = {
+  image:
+    'https://images.unsplash.com/photo-1621636723658-a062df4cbb2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+  time: '13:00',
+  location: 'Площадка «8/3»',
+  title: 'Что я узнал',
+  description: 'Гости расскажут о своём творческом и организационном опыте.',
+  playwright: 'Ольга Казакова',
+  direct: 'Катя Ганюшина',
+};
+
+
 export const Event_Сard = Template.bind({});
 Event_Сard.args = {
   image:
