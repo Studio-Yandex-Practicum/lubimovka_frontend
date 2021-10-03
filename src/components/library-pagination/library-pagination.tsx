f6e6f2fda3fbc290af0d1f3d9cd6d11092e76d08
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-10-03 17:07:32 +0400

    feat: добавить обработку клика на букву

--- src/components/library-pagination/library-pagination.tsx
+++ src/components/library-pagination/library-pagination.tsx
@@ -1,33 +1,54 @@
-import { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import cn from 'classnames';
 
 import style from './library-pagination.module.css';
 
-const letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
+const mockLetters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
   'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я'];
 
-const authors = ['Августеняк Екатерина', 'Александрин Егор'];
+const mockAuthors = ['Августеняк Екатерина', 'Александрин Егор', 'Борисов Борис', 'Фёдоров Фёдор'];
 
-const LibraryPagination: FC = () => {
+interface LibraryPaginationProps {
+  letters: string[]
+}
+
+const LibraryPagination: FC<LibraryPaginationProps> = ({letters = mockLetters}) => {
+  const [authorsState, setAuthorsState] = useState({
+    authors: [''],
+    quantity: 0
+  });
+
+  const [letter, setLetter] = useState('');
+
+  useEffect(() => {
+    if (letter) {
+      const currentAuthors = mockAuthors.filter((el) => el.startsWith(letter));
+      setAuthorsState({
+        authors: currentAuthors,
+        quantity: currentAuthors.length
+      });
+    }
+  }, [letter]);
+
   return (
     <div className={style.container}>
       <form className={style.letters}>
         {letters.map((el, index) => (
-          <div key={index} className={style.letter}>
+          <div key={index} className={cn(style.letter,{ [style.letterActive]: letter === el})}>
             <label htmlFor={el} className={style.label}>{el}</label>
-            <input type="radio" name='letter' id={el} value={el} className={style.inputRadio}/>
+            <input onClick={() => setLetter(el)} type="radio" name='letter' id={el} value={el} className={style.inputRadio}/>
           </div>
         ))}
       </form>
       <div className={style.authors}>
         <ul className={style.authorsList}>
-          {authors.map((el, index) => (
+          {authorsState.authors.length > 0 && authorsState.authors.map((el, index) => (
             <li key={index} className={style.author}>{el}</li>
           ))}
         </ul>
         <div className={style.chosenLetter}>
-          <p className={style.bigLetter}>А</p>
-          <p className={style.authorsQuantity}>16</p>
+          <p className={style.bigLetter}>{letter}</p>
+          <p className={style.authorsQuantity}>{letter && String(authorsState.quantity)}</p>
         </div>
       </div>
     </div>
