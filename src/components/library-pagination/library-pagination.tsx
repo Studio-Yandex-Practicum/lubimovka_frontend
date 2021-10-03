db5a4cc6f6b77beaccfcddedfd66a272d80bf83d
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-10-03 12:19:07 +0400

    feat: добавить навигационную панель с буквами

--- src/components/library-pagination/library-pagination.tsx
+++ src/components/library-pagination/library-pagination.tsx
@@ -3,9 +3,21 @@ import cn from 'classnames';
 
 import style from './library-pagination.module.css';
 
+const letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
+  'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я'];
+
 const LibraryPagination: FC = () => {
   return (
-    <div></div>
+    <div className={style.container}>
+      <form className={style.letters}>
+        {letters.map((el, index) => (
+          <div key={index} className={style.letter}>
+            <label htmlFor={el} className={style.label}>{el}</label>
+            <input type="radio" name='letter' id={el} value={el} className={style.inputRadio}/>
+          </div>
+        ))}
+      </form>
+    </div>
   );
 };
 
