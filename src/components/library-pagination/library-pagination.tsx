93d5f1cf1195a3262eeac2977b7e2350a118c249
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-10-03 14:26:55 +0400

    feat: добавить в вёрстку список авторов и выбранную букву

--- src/components/library-pagination/library-pagination.tsx
+++ src/components/library-pagination/library-pagination.tsx
@@ -6,6 +6,8 @@ import style from './library-pagination.module.css';
 const letters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
   'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я'];
 
+const authors = ['Августеняк Екатерина', 'Александрин Егор'];
+
 const LibraryPagination: FC = () => {
   return (
     <div className={style.container}>
@@ -17,6 +19,17 @@ const LibraryPagination: FC = () => {
           </div>
         ))}
       </form>
+      <div className={style.authors}>
+        <ul className={style.authorsList}>
+          {authors.map((el, index) => (
+            <li key={index} className={style.author}>{el}</li>
+          ))}
+        </ul>
+        <div className={style.chosenLetter}>
+          <p className={style.bigLetter}>А</p>
+          <p className={style.authorsQuantity}>16</p>
+        </div>
+      </div>
     </div>
   );
 };
