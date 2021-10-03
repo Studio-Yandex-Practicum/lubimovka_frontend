7d3c16b939519db1d5e7d83a6464f3eb700e0c23
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-10-03 19:23:39 +0400

    fix: поправить функционал

--- src/components/library-pagination/library-pagination.stories.tsx
+++ src/components/library-pagination/library-pagination.stories.tsx
@@ -7,6 +7,11 @@ export default {
   component: LibraryPagination
 } as ComponentMeta<typeof LibraryPagination>;
 
-const Template: ComponentStory<typeof LibraryPagination> = () => <LibraryPagination />;
+const Template: ComponentStory<typeof LibraryPagination> = (args) => <LibraryPagination {...args}/>;
 
 export const Default = Template.bind({});
+
+Default.args = {
+  letters: ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
+    'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я']
+};
--- src/components/library-pagination/library-pagination.tsx
+++ src/components/library-pagination/library-pagination.tsx
@@ -3,52 +3,46 @@ import cn from 'classnames';
 
 import style from './library-pagination.module.css';
 
-const mockLetters = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н',
-  'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я'];
-
 const mockAuthors = ['Августеняк Екатерина', 'Александрин Егор', 'Борисов Борис', 'Фёдоров Фёдор'];
 
 interface LibraryPaginationProps {
   letters: string[]
 }
 
-const LibraryPagination: FC<LibraryPaginationProps> = ({letters = mockLetters}) => {
-  const [authorsState, setAuthorsState] = useState({
-    authors: [''],
-    quantity: 0
-  });
-
+const LibraryPagination: FC<LibraryPaginationProps> = ({letters}) => {
+  const [authors, setAuthors] = useState(['']);
   const [letter, setLetter] = useState('');
+
+  //После появления API внутри useEffect при обновлении letter будет производиться
+  //fetch запрос и стэйт authors будет обновляться данными с сервера.
+  //Будет что-то вроде GET `https://graph.microsoft.com/v1.0/users?$filter=startswith(givenName, ${letter})`
 
   useEffect(() => {
     if (letter) {
       const currentAuthors = mockAuthors.filter((el) => el.startsWith(letter));
-      setAuthorsState({
-        authors: currentAuthors,
-        quantity: currentAuthors.length
-      });
+      setAuthors(currentAuthors);
     }
   }, [letter]);
 
   return (
     <div className={style.container}>
-      <form className={style.letters}>
+      <div className={style.letters}>
         {letters.map((el, index) => (
           <div key={index} className={cn(style.letter,{ [style.letterActive]: letter === el})}>
             <label htmlFor={el} className={style.label}>{el}</label>
             <input onClick={() => setLetter(el)} type="radio" name='letter' id={el} value={el} className={style.inputRadio}/>
           </div>
         ))}
-      </form>
+      </div>
       <div className={style.authors}>
         <ul className={style.authorsList}>
-          {authorsState.authors.length > 0 && authorsState.authors.map((el, index) => (
+          {authors.length > 0 && authors.map((el, index) => (
             <li key={index} className={style.author}>{el}</li>
           ))}
         </ul>
         <div className={style.chosenLetter}>
           <p className={style.bigLetter}>{letter}</p>
-          <p className={style.authorsQuantity}>{letter && String(authorsState.quantity)}</p>
+          <p className={style.authorsQuantity}>{letter && String(authors.length)}</p>
         </div>
       </div>
     </div>
