9e407ed0ca4e28ef6b62b8006aeefb5372941da8
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-10-04 15:40:43 +0300

    feat: добавить возможность выбора режима первой карточки

--- src/components/ui/blog-card/blog-card.tsx
+++ src/components/ui/blog-card/blog-card.tsx
@@ -2,20 +2,23 @@ import React, { FC } from 'react';
 import styles from './blog-card.module.css';
 import Link from 'next/link';
 
+import cn from 'classnames';
+
 interface BlogCardProps {
   image: string;
   author: string;
   heading: string;
   description: string;
   link: string;
+  firstCardSizeMode?: 'regular' | 'big';
 }
 
 export const BlogCard: FC<BlogCardProps> = (props) => {
-  const { image, author, heading, description, link } = props;
+  const { image, author, heading, description, link, firstCardSizeMode = 'regular' } = props;
 
   return (
     <Link href={link}>
-      <a className={styles.cardLinkWrapper}>
+      <a className={cn(styles.cardLinkWrapper, styles[firstCardSizeMode])}>
         <li className={styles.card}>
           <div className={styles.imgContainer}>
             <img className={styles.img} src={image} alt={`изображение статьи: ${heading}`} />
