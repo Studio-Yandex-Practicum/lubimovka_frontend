090925112f4f8ba3ca6146ba6af026b233400d89
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-06 22:45:17 +0300

    fix: delete global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from 'next/app';
 import 'keen-slider/keen-slider.min.css';
 import 'shared/styles/fonts.css';
 import 'shared/styles/palette.css';
-import 'shared/styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
