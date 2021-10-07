0740716aa5b12045b378e3059d7e03ac2a616c8e
Ilya Borodulin <borodulinid@gmail.com>
2021-10-07 17:33:50 +0300

    feat: Storybook для компонента ArticleSection

---
+++ src/components/ui/article-section/article-section.stories.tsx
@@ -0,0 +1,57 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ArticleSection } from './article-section';
+
+export default {
+  title: 'UI/ArticleSection',
+  component: ArticleSection,
+  decorators: [
+    (Story) => (
+      <div style={{margin: '0 auto', maxWidth: '1440px'}}>
+        <Story/>
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof ArticleSection>;
+
+const Template: ComponentStory<typeof ArticleSection> = (args) => {
+  return (
+    <ArticleSection {...args}>
+      <ArticleSection.Title style={{ maxWidth: '360px' }}>
+        {'Заголовок секции с контентом'}
+      </ArticleSection.Title>
+      <div
+        style={{
+          marginTop: '48px',
+          marginLeft: '24px',
+          width: '100%',
+          display: 'flex',
+          gap: '30px',
+          overflowX: 'auto'
+        }}
+      >
+        {Array.from(Array(5)).map((item, idx) => (
+          <div
+            key={idx}
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '240px',
+              height: '300px',
+              backgroundColor: 'var(--light-green)',
+              flexShrink: 0,
+            }}
+          >
+            {'Пример контента'}
+          </div>
+        ))}
+      </div>
+    </ArticleSection>
+  );
+};
+
+export const Default = Template.bind({});
+Default.parameters = {
+  layout: 'fullscreen'
+};
