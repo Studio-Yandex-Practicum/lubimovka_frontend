548f9583f5dbcbe331d9ba2ef8903c864243e619
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-07 12:58:13 +0300

    style: changes in code formatting in home and archive

--- src/components/main-archive/main-archive.tsx
+++ src/components/main-archive/main-archive.tsx
@@ -4,7 +4,9 @@ import styles from './main-archive.module.css';
 
 export const MainArchive: FC = () => {
   return (
-    <section className={styles.section}><h2 className={styles.title}>Компонент MainArchive</h2></section>
+    <section className={styles.section}>
+      <h2 className={styles.title}>Компонент MainArchive</h2>
+    </section>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,28 @@
-import {NextPage} from 'next';
+import { NextPage } from 'next';
 import Head from 'next/head';
 import cn from 'classnames/bind';
 
-import {MainTitle} from 'components/main-title';
-import {MainEvents} from 'components/main-events';
-import {MainAside} from 'components/main-aside';
-import {MainBanners} from 'components/main-banners';
-import {MainPlatforms} from 'components/main-platforms';
-import {MainArchive} from 'components/main-archive';
-import {MainShortList} from 'components/main-shortList';
+import { MainTitle } from 'components/main-title';
+import { MainEvents } from 'components/main-events';
+import { MainAside } from 'components/main-aside';
+import { MainBanners } from 'components/main-banners';
+import { MainPlatforms } from 'components/main-platforms';
+import { MainArchive } from 'components/main-archive';
+import { MainShortList } from 'components/main-shortList';
 
 import styles from './index.module.css';
+
 const cx = cn.bind(styles);
 
 interface IMainPageProps {
-  title: string,
-  metaTitle: string,
-  events: boolean,
-  aside: boolean,
-  banners: boolean,
-  archive: boolean,
-  platforms: boolean,
-  shortList: boolean,
+  title: string;
+  metaTitle: string;
+  events: boolean;
+  aside: boolean;
+  banners: boolean;
+  archive: boolean;
+  platforms: boolean;
+  shortList: boolean;
 }
 
 export const getStaticProps = () => {
@@ -35,29 +36,37 @@ export const getStaticProps = () => {
       platforms: true,
       archive: true,
       shortList: true,
-    }
+    },
   };
 };
 
-const Home: NextPage<IMainPageProps> = (props : IMainPageProps) => {
-  const {title, metaTitle, events, aside, banners, platforms, archive, shortList} = props;
+const Home: NextPage<IMainPageProps> = (props: IMainPageProps) => {
+  const {
+    title,
+    metaTitle,
+    events,
+    aside,
+    banners,
+    platforms,
+    archive,
+    shortList,
+  } = props;
   return (
     <>
       <Head>
         <title>{metaTitle}</title>
       </Head>
       <main className={cx('main')}>
-        {title && <MainTitle title={title}/>}
-        {events && <MainEvents/>}
-        {aside && <MainAside/>}
-        {banners && <MainBanners/>}
-        {platforms && <MainPlatforms/>}
-        {shortList && <MainShortList/>}
-        {archive && <MainArchive/>}
+        {title && <MainTitle title={title} />}
+        {events && <MainEvents />}
+        {aside && <MainAside />}
+        {banners && <MainBanners />}
+        {platforms && <MainPlatforms />}
+        {shortList && <MainShortList />}
+        {archive && <MainArchive />}
       </main>
     </>
   );
 };
-
 
 export default Home;
