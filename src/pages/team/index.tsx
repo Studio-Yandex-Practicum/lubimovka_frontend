34f6e0ec94356e9e1dbb936a6bcdd278a28760c9
Irina Pupkova <pupkova.ia@yandex.ru>
2021-10-09 01:39:17 +0500

    feat: добавить отрисовку карточек на страницу, инфо-блок для  связя связи

--- src/pages/team/index.tsx
+++ src/pages/team/index.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from 'next';
 
-import volunteersCards from './assets/mock-data.json';
+import TeamSection from '../../components/team-section';
+
+import TeamData from './assets/mock-data.json';
 
 const Team: NextPage = () => (
   <>
+    {TeamData.map(data => <TeamSection key={data.id} data={data}/>)}
   </>
 );
 
