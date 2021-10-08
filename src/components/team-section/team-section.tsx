bcdad443b4f7f3c9acf9633e6cc7ac5aa85bacc9
Irina Pupkova <pupkova.ia@yandex.ru>
2021-10-08 18:25:07 +0500

    feat: добавить компонент страницы team

---
+++ src/components/team-section/team-section.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import cn from 'classnames';
+
+interface ITeamSectionProps {
+  content: {
+    id: number
+    name: string
+    link: string
+    response: string
+  }
+}
+
+const TeamSection: FC<ITeamSectionProps> = ({ content }) => {
+
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default TeamSection;
