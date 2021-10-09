21a301ea217fab28d59f4603869ac9305cdda14f
Ilya Borodulin <borodulinid@gmail.com>
2021-10-09 19:26:38 +0300

    refactor: Убраны мок-карточки, добавлен пропс type

--- src/components/ui/section/section.stories.tsx
+++ src/components/ui/section/section.stories.tsx
@@ -16,34 +16,14 @@ export default {
 
 const Template: ComponentStory<typeof Section> = (args) => {
   return (
-    <Section {...args} title={'Заголовок секции с контентом'}>
+    <Section {...args} type='plays' title={'Заголовок секции с контентом'}>
       <div
         style={{
-          marginTop: '48px',
-          marginLeft: '24px',
           width: '100%',
-          display: 'flex',
-          gap: '30px',
-          overflowX: 'auto'
+          height: '400px',
+          backgroundColor: 'grey'
         }}
-      >
-        {Array.from(Array(5)).map((item, idx) => (
-          <div
-            key={idx}
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              width: '240px',
-              height: '300px',
-              backgroundColor: 'var(--light-green)',
-              flexShrink: 0,
-            }}
-          >
-            {'Пример контента'}
-          </div>
-        ))}
-      </div>
+      />
     </Section>
   );
 };
--- src/components/ui/section/section.tsx
+++ src/components/ui/section/section.tsx
@@ -5,15 +5,16 @@ import styles from './section.module.css';
 const cx = classNames.bind(styles);
 
 interface ISectionProps extends HTMLAttributes<HTMLElement> {
+  type: 'plays' | 'persons';
   title: string;
   children: ReactNode;
 }
 
 export const Section = (props: ISectionProps): JSX.Element => {
-  const { title, children, ...restProps } = props;
+  const { type, title, children, ...restProps } = props;
 
   return (
-    <section className={cx('section')} {...restProps}>
+    <section className={cx('section', type)} {...restProps}>
       <h2 className={cx('title')}>{title}</h2>
       {children}
     </section>
