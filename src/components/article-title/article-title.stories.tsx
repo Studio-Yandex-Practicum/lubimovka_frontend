5a6147ff65caf486279fc4dd28bbd5aa71191527
anastasia <nastja1994@yandex.ru>
2021-10-09 10:06:48 +0300

    feat: реализовать резиновую верстку

--- src/components/article-title/article-title.stories.tsx
+++ src/components/article-title/article-title.stories.tsx
@@ -19,3 +19,13 @@ BlogTitle.args = {
   author: 'Лара Бессмертная',
   imgLink: 'https://littletravel.ru/wp-content/uploads/2020/01/1530726597_pg-9238.jpg'
 };
+
+export const NewsTitle = Template.bind({});
+NewsTitle.args = {
+  isBlog: false,
+  title: '«Человек является источником жеста, смысла, правды»',
+  description: 'МК «Документальность в современном театре и современном искусстве»',
+  date: '05 октября 2020',
+  author: 'Лара Бессмертная',
+  imgLink: 'https://littletravel.ru/wp-content/uploads/2020/01/1530726597_pg-9238.jpg'
+};
