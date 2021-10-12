80ae3ffec472c1b270b323552ac9446a4bf4428a
Tatiana S <tatyana-selyuk@yandex.com>
2021-10-12 19:52:00 +0300

    chore: Разработать страницу автора

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import 'keen-slider/keen-slider.min.css';
 import 'shared/styles/fonts.css';
 import 'shared/styles/palette.css';
 import 'shared/styles/vars.css';
+import 'shared/styles/normalize.css';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
