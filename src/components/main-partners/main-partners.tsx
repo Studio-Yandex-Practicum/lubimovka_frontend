0bf5b28d4f11e6b4307691fa33f47edad8743740
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-12 14:57:05 +0300

    feat: use image from next, put mock images in public

--- src/components/main-partners/main-partners.tsx
+++ src/components/main-partners/main-partners.tsx
@@ -1,18 +1,37 @@
 import { FC } from 'react';
-import { Section } from 'components/section';
+import Link from 'next/link';
+import Image from 'next/image';
 import cn from 'classnames/bind';
 
-const cx = cn.bind(styles);
+import { Section } from 'components/section';
 
 import styles from './main-partners.module.css';
 import data from './assets/mock-data.json';
+
+const cx = cn.bind(styles);
 
 export const MainPartners: FC = () => {
   return (
     <div className={cx('container')}>
       {data.map((partners) => (
-        <Section type="partners" title={partners.title} key={partners.id}>
-          <p>Содержание</p>
+        <Section
+          type="partners"
+          title={partners.title}
+          key={partners.id}
+          component={partners.id === 1 ? 'h2' : 'h3'}
+        >
+          <ul>
+            {partners.content.map((partner) => (
+              <li key={partner.id}>
+                <Link href={partner.link}>
+                  <a>
+                    <Image src={partner.image} width="100px" height="100px" />
+                    {partner.text && <p>{partner.text}</p>}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
         </Section>
       ))}
     </div>
