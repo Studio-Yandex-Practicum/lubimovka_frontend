525da697b74e4d6b9e9c8acca8329f9e3544d2ad
Ilya Borodulin <borodulinid@gmail.com>
2021-10-13 13:55:25 +0300

    refactor: В storybook Section добавлены карточки

--- src/components/section/section.stories.tsx
+++ src/components/section/section.stories.tsx
@@ -1,10 +1,14 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Section } from './section';
+import { BasicPlayCardList } from 'components/ui/basic-play-card';
+
+import { Default as CardListDefault } from 'components/ui/basic-play-card/list/basic-play-card-list.stories';
 
 export default {
   title: 'Components/Section',
   component: Section,
+  subcomponents: { BasicPlayCardList },
   decorators: [
     (Story) => (
       <div style={{margin: '0 auto', maxWidth: '1440px'}}>
@@ -14,21 +18,18 @@ export default {
   ],
 } as ComponentMeta<typeof Section>;
 
-const Template: ComponentStory<typeof Section> = (args) => {
+const Template: ComponentStory<typeof Section> = ({ ...args }) => {
   return (
-    <Section {...args} type='plays' title={'Заголовок секции с контентом'}>
-      <div
-        style={{
-          width: '100%',
-          height: '400px',
-          backgroundColor: 'grey'
-        }}
-      />
-    </Section>
+    <Section {...args} />
   );
 };
 
 export const Default = Template.bind({});
 Default.parameters = {
   layout: 'fullscreen'
+};
+Default.args = {
+  children: <CardListDefault {...CardListDefault.args} />,
+  type: 'plays',
+  title: 'Заголовок секции с контентом'
 };
