271fa00d8d6bf3f634c52516de30befd539af85d
Ilya Borodulin <borodulinid@gmail.com>
2021-10-13 13:54:05 +0300

    refactor: пропс component переименован в titleTag

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -7,7 +7,7 @@ const cx = classNames.bind(styles);
 interface ISectionProps extends HTMLAttributes<HTMLElement> {
   type: 'plays' | 'persons';
   title: string;
-  component?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  titleTag?: 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   children: ReactNode;
 }
 
@@ -15,14 +15,14 @@ export const Section = (props: ISectionProps): JSX.Element => {
   const {
     type,
     title,
-    component: Component = 'h2',
+    titleTag: TitleTag = 'h2',
     children,
     ...restProps
   } = props;
 
   return (
     <section className={cx('section', type)} {...restProps}>
-      <Component className={cx('title')}>{title}</Component>
+      <TitleTag className={cx('title')}>{title}</TitleTag>
       {children}
     </section>
   );
