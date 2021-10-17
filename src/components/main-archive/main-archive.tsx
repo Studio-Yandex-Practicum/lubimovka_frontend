08127a6f59f2876afba4bbb2a1ab50a3fdd49d76
Rustam <Rustamaaaa@bk.ru>
2021-10-17 15:15:31 +0300

    fix/Убрал лишний компонент из ветки

--- src/components/main-archive/main-archive.tsx
+++ src/components/main-archive/main-archive.tsx
@@ -6,7 +6,6 @@ export const MainArchive: FC = () => {
   return (
     <section className={styles.section}>
       <h2 className={styles.title}>Компонент MainArchive</h2>
-      <iframe id="ytplayer" width="540" height="258" src="https://www.youtube.com/embed/iAJTBxq2WZs" frameBorder="0" allowFullScreen /> 
     </section>
   );
 };
