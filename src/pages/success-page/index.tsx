31d5c643786d059a278cf45310a8c22222f5226d
Ron Winchester <RonWinchester@yandex.ru>
2021-10-18 23:08:28 +0300

    feat: собрал header для страницы

--- src/pages/success-page/index.tsx
+++ src/pages/success-page/index.tsx
@@ -1,6 +1,44 @@
 import { NextPage } from 'next';
 import { PlayProposalSuccess } from 'components/play-proposal-success';
 import titleData from './assets/mock-title-success-data.json';
-const SuccessPage: NextPage = () => <PlayProposalSuccess title={titleData.title} />;
+import demoItems from './assets/mock-header-success-data.json';
+
+import { Menu } from 'components/ui/menu';
+import { Logotype } from 'components/logotype';
+import { Button } from 'components/ui/button';
+
+
+const SuccessPage: NextPage = () => (
+  <>
+    <header>
+      <Logotype href="/"></Logotype>
+      <Menu type="main-navigation">
+        {demoItems[0].map((item) => (
+          <Menu.Item key={item.text} href={item.href}>
+            {item.text}
+          </Menu.Item>
+        ))}
+        {demoItems[1].map((item) => (
+          <Menu.Item key={item.text} href={item.href}>
+            {item.text}
+          </Menu.Item>
+        ))}
+      </Menu>
+      <Button
+        label="Поддержать"
+        view="primary"
+        icon="plus"
+        iconPlace="left"
+        isLink={false}
+        align="center"
+        width="165px"
+        size= 'l'
+        border= 'bottomLeft'
+      ></Button>
+    </header>
+
+    <PlayProposalSuccess title={titleData.title} />
+  </>
+);
 
 export default SuccessPage;
