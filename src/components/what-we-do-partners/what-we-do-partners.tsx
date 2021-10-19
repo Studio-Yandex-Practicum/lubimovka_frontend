1b14e43e9dcb13c4deb805cf2be7dc08cff49c62
Rustam <Rustamaaaa@bk.ru>
2021-10-19 22:22:19 +0300

    fix/Добавил правки

--- src/components/what-we-do-partners/what-we-do-partners.tsx
+++ src/components/what-we-do-partners/what-we-do-partners.tsx
@@ -5,9 +5,6 @@ import styles from './what-we-do-partners.module.css';
 
 import { Partners } from 'components/partners';
 
-// interface IWhatWeDoHeaderProps {
-// }
-
 export const WhatWeDoPartners: FC = (): JSX.Element => {
   return (
     <section className={ cn(styles.partners) }>
