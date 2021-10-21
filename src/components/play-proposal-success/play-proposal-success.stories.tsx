b813728653f3e164570c474bc3c4c49f8e66df56
Ron Winchester <RonWinchester@yandex.ru>
2021-10-21 20:54:51 +0300

    fix: состояние после ребейза

--- src/components/play-proposal-success/play-proposal-success.stories.tsx
+++ src/components/play-proposal-success/play-proposal-success.stories.tsx
@@ -9,7 +9,3 @@ export default {
 const Template: ComponentStory<typeof PlayProposalSuccess> = () => <PlayProposalSuccess/>;
 
 export const Default = Template.bind({});
-<<<<<<< HEAD
-=======
-
->>>>>>> f5a71d8 (fix: захардкодил title в компоненте)
