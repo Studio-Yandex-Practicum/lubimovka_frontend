9e9a47346635d1fc5a73d05598347c164cda28fd
Ron Winchester <RonWinchester@yandex.ru>
2021-10-21 19:10:16 +0300

    fix: убрал обертку для img

--- src/components/play-proposal-success/play-proposal-success.tsx
+++ src/components/play-proposal-success/play-proposal-success.tsx
@@ -3,19 +3,21 @@ import classNames from 'classnames/bind';
 import styles from './play-proposal-success.module.css';
 
 interface IPlayProposalSuccessProps {
-  title: string,
+  title: string;
 }
 
 const cx = classNames.bind(styles);
 
-export const PlayProposalSuccess: FC<IPlayProposalSuccessProps> = (props)=> {
-  const {title} = props;
+export const PlayProposalSuccess: FC<IPlayProposalSuccessProps> = (props) => {
+  const { title } = props;
   return (
     <article className={cx('container')}>
       <h1 className={cx('title')}>{title}</h1>
-      <div className={cx('image-conatiner')}>
-        <img src='/image/play-proposal-success/PlayProposalSuccessImage.png' alt='Успешная отправка' className={cx('image')}/>
-      </div>
+      <img
+        src="/image/play-proposal-success/PlayProposalSuccessImage.png"
+        alt="Успешная отправка"
+        className={cx('image')}
+      />
     </article>
   );
 };
