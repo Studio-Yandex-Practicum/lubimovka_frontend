41375b74caf69c01981f5907437cfdf53e986cb2
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-10-23 11:26:33 -0500

    feat: добавление кастомных вьюпортов в strorybook для кнопки мобильного меню

--- src/components/ui/burger-button/burger-button.stories.tsx
+++ src/components/ui/burger-button/burger-button.stories.tsx
@@ -2,9 +2,42 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { BurgerButton } from './burger-button';
 
+const customViewports = {
+  iphone6: {
+    name: 'iPhone 6/7/8',
+    styles: {
+      height: '667px',
+      width: '375px',
+    },
+    type: 'mobile',
+  },
+  iphone6p: {
+    name: 'iPhone 6/7/8 Plus',
+    styles: {
+      height: '736px',
+      width: '414px',
+    },
+    type: 'mobile',
+  },
+  ipad: {
+    name: 'iPad',
+    styles: {
+      height: '1024px',
+      width: '768px',
+    },
+    type: 'tablet',
+  },
+};
+
 export default {
   title: 'UI/BurgerButton',
   component: BurgerButton,
+  parameters: {
+    viewport: {
+      viewports: customViewports,
+      defaultViewport: 'iphone6p'
+    },
+  },
 
 } as ComponentMeta<typeof BurgerButton>;
 
