4103145aa121d998d2ace5fc77aeab84bcb64e5f
Rustam <Rustamaaaa@bk.ru>
2021-10-23 17:03:47 +0300

    fix/Вернул все как было

--- src/components/what-we-do-contacts/what-we-do-contacts.tsx
+++ src/components/what-we-do-contacts/what-we-do-contacts.tsx
@@ -9,18 +9,12 @@ interface IWhatWeDoContactsProps {
   data: {
     id: number
     title: string
-    description1: string
-    description2: string
-    director: {
-      id: number
-      text: string
-      contacts: string
-    }
+    desc: string
   }
 }
 
 export const WhatWeDoContacts: FC<IWhatWeDoContactsProps> = ({ data }): JSX.Element => {
-  const { title, description1, description2, director } = data;
+  const { title, desc } = data;
 
   return (
     <section className={ cn(styles.contacts) }>
@@ -32,9 +26,7 @@ export const WhatWeDoContacts: FC<IWhatWeDoContactsProps> = ({ data }): JSX.Elem
           { <Icon glyph='asterisk' fill='black'
             className={ styles.asterisk } /> }
         </span>
-        { description1 + ' ' }
-        <a href='#'>{ director.text }</a>
-        { ' ' + description2 }
+        { desc }
       </p>
     </section>
   );
