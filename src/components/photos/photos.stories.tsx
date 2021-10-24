c85ccef60ea8ae30677c6ccdf60b6f9252024395
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-10-24 21:31:04 +0300

    feat: настроен просмотр в storybook

--- src/components/photos/photos.stories.tsx
+++ src/components/photos/photos.stories.tsx
@@ -1,26 +1,10 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import * as NextImage from 'next/image';
-import { ImageProps } from 'next/image';
 
 import { Photos } from './photos';
-
-const OriginalNextImage = NextImage.default;
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props: ImageProps) => <OriginalNextImage {...props} unoptimized />,
-});
 
 export default {
   title: 'Components/Photos',
   component: Photos,
-  decorators: [
-    (Story) => (
-      <div style={{ margin: '0 auto', maxWidth: '1440px' }}>
-        <Story />
-      </div>
-    ),
-  ],
 } as ComponentMeta<typeof Photos>;
 
 const Template: ComponentStory<typeof Photos> = (args) => {
