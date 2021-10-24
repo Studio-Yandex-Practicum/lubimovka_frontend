8fa46cbeadb80a2e5b797a43d705798481ac6cf6
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-10-24 10:26:44 -0500

    feat: создание страницы Project

---
+++ src/pages/project/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+import { AppLayout } from 'components/app-layout';
+
+const Project: NextPage = () => (
+  <AppLayout>
+    <Head>
+      <title>{'project'}</title>
+    </Head>
+    <main></main>
+  </AppLayout>
+);
+
+export default Project;
