00e17f978b189d75b397f3945e4566eefe6943fa
Ilya Borodulin <borodulinid@gmail.com>
2021-10-27 13:34:04 +0300

    refactor: TextField разделен на TextInput и TextArea

---
+++ src/components/ui/text-input/text-input.tsx
@@ -0,0 +1,31 @@
+import classNames from 'classnames/bind';
+
+import styles from './text-input.module.css';
+const cx = classNames.bind(styles);
+
+interface ITextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  ariaLabel: string;
+  errorMessage?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const TextInput = (props: ITextInputProps): JSX.Element => {
+  const {
+    ariaLabel,
+    errorMessage = '',
+    onChange,
+    ...restProps
+  } = props;
+
+  return (
+    <>
+      <input
+        className={cx('textInput', { error: errorMessage })}
+        aria-label={ariaLabel}
+        onChange={onChange}
+        {...restProps}
+      />
+      {errorMessage && <span className={cx('errorMessage')}>{errorMessage}</span>}
+    </>
+  );
+};
