5c810955197c8d727a324377708355cd18f7ca7c
gremwiz1 <gremwiz@yandex.ru>
2021-10-28 02:57:05 +0300

    feat: make component MainShortList

--- src/components/main-page/shortList/main-shortList.tsx
+++ src/components/main-page/shortList/main-shortList.tsx
@@ -1,13 +1,36 @@
+import { BasicPlayCard } from 'components/ui/basic-play-card';
 import { FC } from 'react';
 
 import styles from './main-shortList.module.css';
+interface PlaysData {
+  title: string
+  city: string
+  year: string
+  linkView: string,
+  linkDownload: string
+  author: AuthorData
+}
+interface AuthorData {
+  id: number
+  name: string
+}
+interface IMainShortList {
+  data: {
+    plays: PlaysData[]
+    title: string
+  }
+}
 
 
-
-export const MainShortList: FC = () => {
+export const MainShortList: FC<IMainShortList>= ({ data }) => {
   return (
     <section className={styles.section}>
-      <h2 className={styles.title}>Компонент MainShortList</h2>
+      <h2 className={styles.title}>{data.title}</h2>
+      <div className={styles.plays}>
+        {data.plays.map((item) => (
+          <BasicPlayCard play={item} author={item.author} key={item.author.id}/>
+        ))}
+      </div>
     </section>
   );
 };
