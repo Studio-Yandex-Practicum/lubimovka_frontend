a3a79413137cff61d7b9fb44a91f349cc3dc3a60
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-10-28 16:30:25 -0500

    feat: добавление компонента ProjectAbout

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -3,8 +3,10 @@ import Head from 'next/head';
 
 import { AppLayout } from 'components/app-layout';
 import { ProjectHeader } from 'components/project-header';
+import { ProjectAbout } from 'components/project-about';
 
 import headerData from './assets/mock-project-header-data.json';
+import aboutData from './assets/mock-project-about-data.json';
 
 const Project: NextPage = () => (
   <AppLayout>
@@ -13,6 +15,7 @@ const Project: NextPage = () => (
     </Head>
     <main>
       <ProjectHeader data={ headerData } />
+      <ProjectAbout data={ aboutData } />
     </main>
   </AppLayout>
 );
