e902eed4661281ea9d4591df5aaeda69e9704994
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-10-28 14:47:56 -0500

    fix: удаление ссылки 'проекты' (часть другого компонента)

--- src/components/project-header/project-header.tsx
+++ src/components/project-header/project-header.tsx
@@ -3,8 +3,6 @@ import { Url } from 'shared/types/common';
 
 import styles from './project-header.module.css';
 
-import { Button } from '../ui/button';
-
 interface IProjectHeaderProps {
   data: {
     title: string,
@@ -22,19 +20,6 @@ export const ProjectHeader = ({ data }: IProjectHeaderProps): JSX.Element => {
 
   return (
     <div className={cx('container')}>
-      <Button
-        view='primary'
-        width='120px'
-        className={cx('button')}
-        align='end'
-        size='s'
-        iconPlace='right'
-        icon='arrow-left'
-        label='Проекты'
-        border='bottomRight'
-        isLink={true}
-        /* href={} */
-      />
       <h1 className={cx('title')}>{title}</h1>
       <p className={cx('desc')}>{desc}<span className={cx('desc-addon')}> {descAddon}</span></p>
       <img
