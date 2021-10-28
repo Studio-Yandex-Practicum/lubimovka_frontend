096b8e598cf3d2a8d3d4699628da6787866b4f52
Ron Winchester <RonWinchester@yandex.ru>
2021-10-28 22:55:39 +0300

    fix: удалили лишний контейнер, переназвал переменную

--- src/components/creators-list/creators-list.tsx
+++ src/components/creators-list/creators-list.tsx
@@ -5,58 +5,30 @@ import styles from './creators-list.module.css';
 const cx = classNames.bind(styles);
 
 export interface ICreatorsListProps {
-
-    playwright: string;
-    translation: string;
-    textAdaptation: string;
-    duration: string;
-    actors: Array<string>;
-
+  playwright: string;
+  translation: string;
+  textAdaptation: string;
+  duration: string;
+  actors: Array<string>;
 }
 
 export const CreatorsList: FC<ICreatorsListProps> = (props) => {
-  const {
-    playwright,
-    translation,
-    textAdaptation,
-    duration,
-    actors
-  } = props;
+  const { playwright, translation, textAdaptation, duration, actors } = props;
 
-  const actorsArr = actors.join(', ');
+  const actorsString = actors.join(', ');
 
-  return <div>
+  return (
     <dl className={cx('dl-class')}>
-      <dt className={cx('dt-class')}>
-      Драматург
-      </dt>
-      <dd className={cx('dd-class')}>
-        {playwright}
-      </dd>
-      <dt className={cx('dt-class')}>
-      Перевод
-      </dt>
-      <dd className={cx('dd-class')}>
-        {translation}
-      </dd>
-      <dt className={cx('dt-class')}>
-      Адаптация текста
-      </dt>
-      <dd className={cx('dd-class')}>
-        {textAdaptation}
-      </dd>
-      <dt className={cx('dt-class')}>
-      Режиссёр
-      </dt>
-      <dd className={cx('dd-class')}>
-        {duration}
-      </dd>
-      <dt className={cx('dt-class')}>
-      Актёры
-      </dt>
-      <dd className={cx('dd-class')}>
-        {actorsArr}
-      </dd>
+      <dt className={cx('dt-class')}>Драматург</dt>
+      <dd className={cx('dd-class')}>{playwright}</dd>
+      <dt className={cx('dt-class')}>Перевод</dt>
+      <dd className={cx('dd-class')}>{translation}</dd>
+      <dt className={cx('dt-class')}>Адаптация текста</dt>
+      <dd className={cx('dd-class')}>{textAdaptation}</dd>
+      <dt className={cx('dt-class')}>Режиссёр</dt>
+      <dd className={cx('dd-class')}>{duration}</dd>
+      <dt className={cx('dt-class')}>Актёры</dt>
+      <dd className={cx('dd-class')}>{actorsString}</dd>
     </dl>
-  </div>;
+  );
 };
