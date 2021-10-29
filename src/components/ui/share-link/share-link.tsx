ec1148953ad697243d43df7ffbae341754a8bb96
Irina Pupkova <pupkova.ia@yandex.ru>
2021-10-29 21:26:22 +0500

    refactor: изменить подход к определению текущего url, внести изменения согласно ревью

--- src/components/ui/share-link/share-link.tsx
+++ src/components/ui/share-link/share-link.tsx
@@ -1,31 +1,48 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import { InfoLink } from '../info-link';
 
 import styles from './share-link.module.css';
 import classNames from 'classnames/bind';
 
+
 const cx = classNames.bind(styles);
 
+const icon = 'arrow-right';
+const iconPlace = 'left';
+const size = 's';
+const border = 'borderBottomLeft';
+
 interface IShareLinkProps {
-  icon: 'arrow-right',
-  iconPlace: 'left',
-  size: 's',
-  border: 'borderBottomLeft',
   className?: string
 }
 
-const facebook = `https://www.facebook.com/sharer.php?src=sp&u=${encodeURI(document.location.href)}%2F&title=${encodeURI(document.title)}&utm_source=share2`;
-const vk = `https://vk.com/share.php?url=${encodeURI(document.location.href)}%2F&title=${encodeURI(document.title)}&utm_source=share2`;
-const tweeter = `https://twitter.com/intent/tweet?text=${encodeURI(document.title)}&url=${encodeURI(document.location.href)}%2F&utm_source=share2`;
-
 export const ShareLink: FC<IShareLinkProps> = (props) => {
-  const { icon, iconPlace, size, border, className } = props;
+  const { className } = props;
+
+  const router = useRouter();
+
+  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    const baseUrl = document.location.href;
+    const title = document.title;
+
+    setUrl(baseUrl);
+    setTitle(title);
+  }, [router.pathname]);
+
+  const facebook = `https://www.facebook.com/sharer.php?src=sp&u=${encodeURI(url)}%2F&title=${encodeURI(title)}&utm_source=share2`;
+  const vk = `https://vk.com/share.php?url=${encodeURI(url)}%2F&title=${encodeURI(title)}&utm_source=share2`;
+  const tweeter = `https://twitter.com/intent/tweet?text=${encodeURI(title)}&url=${encodeURI(url)}%2F&utm_source=share2`;
+
 
   return (
     <div className={cx('container', className)}>
-      <InfoLink href={facebook} label='Fb' icon={icon} iconPlace={iconPlace} size={size} border={border} />
-      <InfoLink href={vk} label='Vk' icon={icon} iconPlace={iconPlace} size={size} border={border} />
-      <InfoLink href={tweeter} label='Twtr' icon={icon} iconPlace={iconPlace} size={size} border={border} />
+      <InfoLink href={facebook} label='Fb' icon={icon} iconPlace={iconPlace} size={size} border={border} className={cx('links')}/>
+      <InfoLink href={vk} label='Vk' icon={icon} iconPlace={iconPlace} size={size} border={border} className={cx('links')}/>
+      <InfoLink href={tweeter} label='Twtr' icon={icon} iconPlace={iconPlace} size={size} border={border} className={cx('links')}/>
     </div>
   );
 };
