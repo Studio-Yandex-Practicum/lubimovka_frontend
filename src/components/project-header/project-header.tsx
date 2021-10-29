76cd89e68cebfde5c3b3bf1a6656c95fec6d0129
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-10-29 16:54:35 -0500

    refactor: переписана композиция страницы проекта

--- src/components/project-header/project-header.tsx
+++ src/components/project-header/project-header.tsx
@@ -4,18 +4,16 @@ import { Url } from 'shared/types/common';
 import styles from './project-header.module.css';
 
 interface IProjectHeaderProps {
-  data: {
-    title: string,
-    intro: string,
-    image: Url,
-    imageDesc?: string,
-  };
+  title: string,
+  intro: string,
+  image: Url,
+  imageDesc?: string,
 }
 
 const cx = classNames.bind(styles);
 
-export const ProjectHeader = ({ data }: IProjectHeaderProps): JSX.Element => {
-  const { title, intro, image, imageDesc } = data;
+export const ProjectHeader = ( props: IProjectHeaderProps): JSX.Element => {
+  const { title, intro, image, imageDesc } = props;
 
   return (
     <section className={cx('container')}>
