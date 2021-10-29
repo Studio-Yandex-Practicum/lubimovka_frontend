1d6af920a88cd6d22b184beff591688f1eb16a9e
Ron Winchester <RonWinchester@yandex.ru>
2021-10-29 21:47:28 +0300

    fix: переименовал данные в интерфейсе,удалил импорт react

--- src/components/creators-list/creators-list.tsx
+++ src/components/creators-list/creators-list.tsx
@@ -5,28 +5,28 @@ import styles from './creators-list.module.css';
 const cx = classNames.bind(styles);
 
 export interface ICreatorsListProps {
-  playwright: string;
-  translation: string;
+  playwrighter: string;
+  translator: string;
   textAdaptation: string;
-  duration: string;
+  director: string;
   actors: Array<string>;
 }
 
 export const CreatorsList: FC<ICreatorsListProps> = (props) => {
-  const { playwright, translation, textAdaptation, duration, actors } = props;
+  const { playwrighter, translator, textAdaptation, director, actors } = props;
 
   const actorsString = actors.join(', ');
 
   return (
     <dl className={cx('dl-class')}>
       <dt className={cx('dt-class')}>Драматург</dt>
-      <dd className={cx('dd-class')}>{playwright}</dd>
+      <dd className={cx('dd-class')}>{playwrighter}</dd>
       <dt className={cx('dt-class')}>Перевод</dt>
-      <dd className={cx('dd-class')}>{translation}</dd>
+      <dd className={cx('dd-class')}>{translator }</dd>
       <dt className={cx('dt-class')}>Адаптация текста</dt>
       <dd className={cx('dd-class')}>{textAdaptation}</dd>
       <dt className={cx('dt-class')}>Режиссёр</dt>
-      <dd className={cx('dd-class')}>{duration}</dd>
+      <dd className={cx('dd-class')}>{director}</dd>
       <dt className={cx('dt-class')}>Актёры</dt>
       <dd className={cx('dd-class')}>{actorsString}</dd>
     </dl>
