45a66f7d63318452d25b48ef9209a5200b8ebabd
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-10-30 22:05:28 +0300

    fix: добавлен <Icon>, исправлен класс блока

--- src/components/project-page-cooperation/project-page-cooperation.tsx
+++ src/components/project-page-cooperation/project-page-cooperation.tsx
@@ -2,20 +2,21 @@ import {FC, HTMLAttributes} from 'react';
 import classNames from 'classnames/bind';
 import styles from './project-page-cooperation.module.css';
 import { Email } from 'shared/types/common';
+import { Icon } from '../ui/icon/icon';
 
 const cx = classNames.bind(styles);
 
-interface IOpenToCoopProps extends HTMLAttributes<HTMLElement> {
+interface IProjectPageCooperationProps extends HTMLAttributes<HTMLElement> {
   email: Email;
 }
 
-export const OpenToCooperation: FC<IOpenToCoopProps> = (props) => {
+export const ProjectPageCooperation: FC<IProjectPageCooperationProps> = (props) => {
   const { email } = props;
 
   return (
-    <div className={cx('container')}>
-      <div className={cx('asterisk')}></div>
-      <h5 className={cx('header')}>Проект открыт к&nbsp;сотрудничеству</h5>
+    <div className={cx('project-page-cooperation')}>
+      <Icon glyph='asterisk' className={cx('asterisk')} />
+      <h3 className={cx('header')}>Проект открыт к&nbsp;сотрудничеству</h3>
       <p className={cx('text')}>Мы находимся в постоянном поиске режиссёров и актеров, заинтересованных в постановке читок.</p>
       <p className={cx('text')}>Пишите на <a className={cx('link')} href={`mailto:${email}`}>{email}</a></p>
     </div>
