12b23cc5bf93cc747eb911fcfc6561452273de95
Vyacheslav Bardakov <Kerjanoid@yandex.ru>
2021-10-30 19:40:59 +0300

    fix: добавлен пробел, заменен <br> на <p>

--- src/components/open-to-cooperation/project-page-cooperation.tsx
+++ src/components/open-to-cooperation/project-page-cooperation.tsx
@@ -1,11 +1,12 @@
 import {FC, HTMLAttributes} from 'react';
 import classNames from 'classnames/bind';
-import styles from './open-to-cooperation.module.css';
+import styles from './project-page-cooperation.module.css';
+import { Email } from 'shared/types/common';
 
 const cx = classNames.bind(styles);
 
 interface IOpenToCoopProps extends HTMLAttributes<HTMLElement> {
-  email: string;
+  email: Email;
 }
 
 export const OpenToCooperation: FC<IOpenToCoopProps> = (props) => {
@@ -15,11 +16,8 @@ export const OpenToCooperation: FC<IOpenToCoopProps> = (props) => {
     <div className={cx('container')}>
       <div className={cx('asterisk')}></div>
       <h5 className={cx('header')}>Проект открыт к&nbsp;сотрудничеству</h5>
-      <p className={cx('text')}>Мы находимся в постоянном поиске режиссёров и актеров, заинтересованных в постановке читок.
-        <br/>
-        <br />Пишите на
-        <a className={cx('link')} href={`mailto:${email}`}>{email}</a>
-      </p>
+      <p className={cx('text')}>Мы находимся в постоянном поиске режиссёров и актеров, заинтересованных в постановке читок.</p>
+      <p className={cx('text')}>Пишите на <a className={cx('link')} href={`mailto:${email}`}>{email}</a></p>
     </div>
   );
 };
