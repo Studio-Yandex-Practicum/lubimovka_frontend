aa7692d01d9ac42adafdea6f6539949e188b3c1c
Konstantin Brovtsev <brovan@yandex.ru>
2021-10-31 17:50:36 +0300

    feat: add aside heading, add scale to button padding

--- src/components/main-page/aside/main-aside.tsx
+++ src/components/main-page/aside/main-aside.tsx
@@ -1,16 +1,33 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import cn from 'classnames/bind';
+
+import { Button } from 'components/ui/button';
 import styles from './main-aside.module.css';
+import data from '../assets/mock-data.json';
 
 const cx = cn.bind(styles);
 
 export const MainAside: FC = () => {
-  const [active, setActive] = useState(false);
-  const handleAside = () => {
-    setActive(!active);
-  };
+  const { title, buttonText, buttonLink } = data.aside;
   return (
-    <aside className={cx('aside', {active})} onClick={handleAside}> <h2 className={cx('title')}>Компонент MainAside</h2> </aside>
+    <aside className={cx('aside')}>
+      <div className={cx('heading')}>
+        <h2 className={cx('title')}>{title}</h2>
+        <div className={cx('buttonContainer')}>
+          <Button
+            label={buttonText}
+            isLink
+            href={buttonLink}
+            width="100%"
+            border="bottomLeft"
+            iconPlace="left"
+            icon="arrow-right"
+            align="start"
+            gap="9px"
+            size="s"
+          />
+        </div>
+      </div>
+    </aside>
   );
 };
-
