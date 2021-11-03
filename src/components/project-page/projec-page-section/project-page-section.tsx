dfa7d396559704508e023308eea5eeaccb928add
Ron Winchester <RonWinchester@yandex.ru>
2021-11-03 23:33:54 +0300

    feat: добавление компонента project-page и project-page-section

---
+++ src/components/project-page/projec-page-section/project-page-section.tsx
@@ -0,0 +1,27 @@
+import { ReactNode, HTMLAttributes } from 'react';
+import classNames from 'classnames/bind';
+
+import styles from './project-page-section.module.css';
+const cx = classNames.bind(styles);
+
+interface IProjecPageSectionProps extends HTMLAttributes<HTMLElement> {
+  type: 'video' | 'photo' | 'performances' | 'plays' | 'person';
+  title: string;
+  children: ReactNode;
+}
+
+export const ProjecPageSection = (props: IProjecPageSectionProps): JSX.Element => {
+  const {
+    type,
+    title,
+    children,
+    ...restProps
+  } = props;
+
+  return (
+    <section className={cx('section', type)} {...restProps}>
+      <h2 className={cx('title')}>{title}</h2>
+      {children}
+    </section>
+  );
+};
