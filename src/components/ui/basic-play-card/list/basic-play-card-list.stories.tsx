c07caa20067ef31fa17057ddd0210b14329c15a7
Anya Ch <annnacha@yandex.ru>
2021-11-04 02:25:19 +0300

    fix: обновление логики резиновой верстки

--- src/components/ui/basic-play-card/list/basic-play-card-list.stories.tsx
+++ src/components/ui/basic-play-card/list/basic-play-card-list.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentMeta, Story } from '@storybook/react';
 
 import { BasicPlayCardList, BasicPlayCard, IBasicPlayCardProps } from '..';
 
-import { Default as CardDefault } from '../basic-play-card.stories';
+import { DefaultPlayCard as CardDefault } from '../basic-play-card.stories';
 
 export default {
   title: 'ui/BasicPlayCardList',
