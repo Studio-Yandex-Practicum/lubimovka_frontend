fed2703a6ae6d5466b89f3eff2f5c3805be2827d
Ron Winchester <RonWinchester@yandex.ru>
2021-11-04 20:42:54 +0300

    feat: подключил mock запросов

--- src/mocks/fetch-mock.ts
+++ src/mocks/fetch-mock.ts
@@ -7,6 +7,7 @@ import partners from './data/partners';
 import performance from './data/performance';
 import contacts from 'mocks/data/contacts';
 import form from 'mocks/data/form';
+import project from './data/project';
 
 fetchMock.config.fallbackToNetwork = true;
 
@@ -17,6 +18,7 @@ mockedFetch
   .get({ matcher: addOriginToApiPath('/info/partners/'), query: { type: 'general' } }, partners.filter(({ type }) => type === 'general'))
   .get({ matcher: new RegExp(addOriginToApiPath('/library/performances/\\d+')) }, performance)
   .get(addOriginToApiPath('/contacts'), contacts)
-  .get(addOriginToApiPath('/form'), form);
+  .get(addOriginToApiPath('/form'), form)
+  .get({ matcher: new RegExp(addOriginToApiPath('/project/\\d+')) }, project);
 
 export default mockedFetch;
--- src/pages/project/[id].tsx
+++ src/pages/project/[id].tsx
@@ -1,40 +1,94 @@
-import { NextPage } from 'next';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import Head from 'next/head';
+import { fetcher } from 'shared/fetcher';
 
 import AppLayout from 'components/app-layout';
 import { ProjectPage } from 'components/project-page';
 import { ProjecPageSection } from 'components/project-page/projec-page-section';
 import { ProjectHeader } from 'components/project-header';
 import { ProjectDescription } from 'components/project-description';
-/* import { PhotoGallery } from 'components/photo-gallery'; */
+import { PhotoGallery } from 'components/photo-gallery';
 import { AnnouncedPlayCard } from 'components/ui/announced-play-card';
 import { BasicPlayCard } from 'components/ui/basic-play-card';
 import { PersonCardList } from 'components/ui/person-card';
 import { PersonCard } from 'components/ui/person-card';
-
-import projectData from './assets/mock-project-data.json';
 import { VideoGallery } from 'components/video-gallery';
 import { VideoGalleryItem } from 'components/video-gallery/video-gallery-item';
 import { AnnouncedPlayCardList } from 'components/ui/announced-play-card/announced-play-card-list';
 import { BasicPlayCardList } from 'components/ui/basic-play-card/list';
 import { ProjectCooperation } from 'components/project-cooperation';
-/* import { PhotoGalleryItem } from 'components/photo-gallery/item'; */
+import { PhotoGalleryItem } from 'components/photo-gallery/item';
+import { Url } from 'shared/types';
 
-const Project: NextPage = () => {
+type ProjectResponse = {
+  titledata: {
+    title: string,
+    intro: string,
+    image: Url,
+    imageDesc: string,
+    description: string
+  },
+  images: {
+    image: Url,
+  }[],
+  videoDescription: string,
+  cardsArr: {
+    date: string;
+    time: string;
+    title: string;
+    playwrightArray: string [];
+    directorArray: string [];
+    eventDescription?:string;
+    buttonLinks: string [];
+    coverResourceUrl?: string;
+    className?: string;
+  }[],
+  basicPlayCard: {
+    play: {
+      title: string;
+      city: string;
+      year: string;
+      linkView: Url;
+      linkDownload: Url;
+    };
+    author: {
+      id: number,
+      name: string;
+    };
+    buttonVisibility?: boolean;
+  }[],
+  personCard: {
+    participant: boolean,
+    name: string,
+    link: string,
+    about?: string,
+    response?: string,
+    handleClick?: React.MouseEventHandler<HTMLButtonElement>,
+  }[],
+  email: string,
+  projectDescription: {
+    title: string,
+    description: string
+  }
+}
+
+const Project = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>): JSX.Element | null => {
+  if (!data) return null;
+
   return (
     <AppLayout>
       <Head>
-        <title>{projectData.titledata.title}</title>
+        <title>{data.titledata.title}</title>
       </Head>
       <ProjectPage>
         <ProjectHeader
-          title={projectData.titledata.title}
-          intro={projectData.titledata.intro}
-          image={projectData.titledata.image}
-          imageDesc={projectData.titledata.imageDesc}
+          title={data.titledata.title}
+          intro={data.titledata.intro}
+          image={data.titledata.image}
+          imageDesc={data.titledata.imageDesc}
         />
         <ProjectDescription>
-          {projectData.titledata.description}
+          {data.titledata.description}
         </ProjectDescription>
         <ProjecPageSection type="video" title="Заголовок блока с видео">
           <VideoGallery>
@@ -42,21 +96,21 @@ const Project: NextPage = () => {
           </VideoGallery>
         </ProjecPageSection>
         <ProjecPageSection type="photo" title="Заголовок блока с фотографиями">
-          {/* <PhotoGallery>
-            {projectData.images.map((photo, idx) => (
+          <PhotoGallery>
+            {data.images.map((photo, idx) => (
               <PhotoGalleryItem
-                image={''}
+                image={photo.image}
                 key={idx}
               ></PhotoGalleryItem>
             ))}
-          </PhotoGallery> */}
+          </PhotoGallery>
         </ProjecPageSection>
         <ProjecPageSection
           type="performances"
           title="Заголовок блока с двумя спектаклями"
         >
           <AnnouncedPlayCardList>
-            {projectData.cardsArr.slice(0, 2).map((card, index) => (
+            {data.cardsArr.slice(0, 2).map((card, index) => (
               <AnnouncedPlayCard
                 key={index}
                 date={card.date}
@@ -72,7 +126,7 @@ const Project: NextPage = () => {
         </ProjecPageSection>
         <ProjecPageSection type="plays" title="Заголовок блока с пьесами">
           <BasicPlayCardList>
-            {projectData.basicPlayCard.map((i, index) => (
+            {data.basicPlayCard.map((i, index) => (
               <BasicPlayCard
                 key={index}
                 play={i.play}
@@ -84,7 +138,7 @@ const Project: NextPage = () => {
         </ProjecPageSection>
         <ProjecPageSection type="person" title="Заголовок блока с персонами">
           <PersonCardList>
-            {projectData.personCard.map((i, index) => (
+            {data.personCard.map((i, index) => (
               <PersonCard
                 key={index}
                 name={i.name}
@@ -97,16 +151,29 @@ const Project: NextPage = () => {
         </ProjecPageSection>
         <ProjecPageSection
           type="through-block"
-          title={projectData.projectDescription.title}
-          description={projectData.projectDescription.description}
+          title={data.projectDescription.title}
+          description={data.projectDescription.description}
           button={true}
         />
         <ProjecPageSection type="cooperation">
-          <ProjectCooperation email={projectData.email}></ProjectCooperation>
+          <ProjectCooperation email={data.email}></ProjectCooperation>
         </ProjecPageSection>
       </ProjectPage>
     </AppLayout>
   );
+};
+
+export const getServerSideProps = async ({ params }: GetServerSidePropsContext) => {
+  if (!params) return { props: {}};
+
+  const { id } = params;
+  const data = await fetcher<ProjectResponse>(`/project/${id}`);
+
+  return {
+    props: {
+      data,
+    },
+  };
 };
 
 export default Project;
