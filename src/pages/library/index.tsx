901a18ca4015afcb5e5370818a94feb4000d4a49
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-11-07 10:30:40 +0400

    feat: обернуть страницу library в AppLayout

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -4,6 +4,7 @@ import LibraryForm from 'components/library-form/library-form';
 import LibraryFilter from 'components/library-filter/library-filter';
 import { BasicPlayCard, BasicPlayCardList, IBasicPlayCardProps } from 'components/ui/basic-play-card';
 import { Menu } from 'components/ui/menu';
+import AppLayout from 'components/app-layout';
 
 import styles from './index.module.css';
 
@@ -23,39 +24,43 @@ const mockCard = {
 
 const items = Array.from(Array(7)).map(() => mockCard);
 
-const Library: NextPage = () => (
-  <div className={styles.content}>
-    <div className={styles.sidebar}>
-      <div className={styles.menu}>
-        <Menu type='history'>
-          <Menu.Item
-            href='/library/plays'
-          >
+const Library: NextPage = () => {
+  return (
+    <AppLayout>
+      <div className={styles.content}>
+        <div className={styles.sidebar}>
+          <div className={styles.menu}>
+            <Menu type='history'>
+              <Menu.Item
+                href='/library/plays'
+              >
         Пьесы
-          </Menu.Item>
-          <Menu.Item
-            href='/library/authors'
-          >
+              </Menu.Item>
+              <Menu.Item
+                href='/library/authors'
+              >
         Авторы
-          </Menu.Item>
-        </Menu>
+              </Menu.Item>
+            </Menu>
+          </div>
+          <LibraryFilter />
+        </div>
+        <section className={styles.section}>
+          <h1 className={styles.title}>Библиотека</h1>
+          <div className={styles.search}>
+            <LibraryForm />
+          </div>
+          <div className={styles.pieces}>
+            <BasicPlayCardList>
+              {(items as IBasicPlayCardProps[]).map((item, idx) => (
+                <BasicPlayCard key={idx} {...item}/>
+              ))}
+            </BasicPlayCardList>
+          </div>
+        </section>
       </div>
-      <LibraryFilter />
-    </div>
-    <section className={styles.section}>
-      <h1 className={styles.title}>Библиотека</h1>
-      <div className={styles.search}>
-        <LibraryForm />
-      </div>
-      <div className={styles.pieces}>
-        <BasicPlayCardList>
-          {(items as IBasicPlayCardProps[]).map((item, idx) => (
-            <BasicPlayCard key={idx} {...item}/>
-          ))}
-        </BasicPlayCardList>
-      </div>
-    </section>
-  </div>
-);
+    </AppLayout>
+  );
+};
 
 export default Library;
