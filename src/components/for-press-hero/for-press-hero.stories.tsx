ba3b485a5364cf2a48aeb5fe2685085c8cc8a000
Anya Ch <annnacha@yandex.ru>
2021-11-08 09:47:15 +0300

    fix: удаление декоратора в storybook

--- src/components/for-press-hero/for-press-hero.stories.tsx
+++ src/components/for-press-hero/for-press-hero.stories.tsx
@@ -6,13 +6,6 @@ import { ForPressHero } from './for-press-hero';
 export default {
   title: 'Components/ForPressHero',
   component: ForPressHero,
-  decorators: [
-    (Story) => (
-      <div style={{margin: '0 auto', maxWidth: '1440px'}}>
-        <Story/>
-      </div>
-    ),
-  ]
 } as ComponentMeta<typeof ForPressHero>;
 
 const Template: ComponentStory<typeof ForPressHero> = (args) => {
