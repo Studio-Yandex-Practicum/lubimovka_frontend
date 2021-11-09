85bdb0e52ee19553e7c8a621eb0408288b722f70
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-11-09 20:44:40 +0300

    refactor(storybook): привести аргументы в соответствие с текущими пропсами компонента

--- src/components/ui/project-card/project-card.stories.tsx
+++ src/components/ui/project-card/project-card.stories.tsx
@@ -11,11 +11,8 @@ const Template: ComponentStory<typeof ProjectCard> = (args) => <ProjectCard {...
 
 export const LubimovkaMore = Template.bind({});
 LubimovkaMore.args = {  
-  data: {
-    link: '/projects/more',
-    imgUrl: 'http://rtcam.ru/images/lubimovka/projects/praktika.jpg',
-    imgAlt: 'Люди в зрительном зале',
-    title: 'Любимовка.Ещё',
-    text: 'Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки. Можно слушать, обсуждать и даже участвовать',
-  },
+  id: 1,
+  title: 'Любимовка.Ещё',
+  description: 'Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки. Можно слушать, обсуждать и даже участвовать',
+  image: 'http://rtcam.ru/images/lubimovka/projects/praktika.jpg',
 };
