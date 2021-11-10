a030800ccd3e114c3bf11b99cd9c371bcba38838
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-11-11 00:05:50 +0300

    fix: удаление лишних модулей

--- src/pages/projects.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next';
-import Projects from 'components/ui/projects/projects/Projects';
-
-const ProjectsGallery: NextPage = () => (
-  <Projects />
-);
-
-export default ProjectsGallery;
