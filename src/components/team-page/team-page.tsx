fff9872616b1d93e3bc1679b8f94e2544617fd51
Ирина Пупкова <pupkova.ia@yandex.ru>
2021-11-12 01:14:39 +0500

    feat: добавить меню по страницам о фестивале

--- src/components/team-page/team-page.tsx
+++ src/components/team-page/team-page.tsx
@@ -3,15 +3,18 @@ import { FC } from 'react';
 import ArtDirectorateData from './assets/mock-data-art.json';
 import FestivalTeamData from './assets/mock-trustees-data.json';
 import VolunteersData from './assets/mock-data-volunteers.json';
+import SubmenuData from './assets/submenu-data.json';
 
 import ArtDirectorateSection from './art-directorate/section/art-directorate-section';
 import FestivalTeamSection from './festival-team/festival-team-section';
 import VolunteersSection from './volunteers/section/volunteers-section';
+import Submenu from './submenu/submenu';
 
 
 const TeamPage: FC = () => {
   return (
     <>
+      <Submenu submenu={SubmenuData}/>
       <ArtDirectorateSection cards={ArtDirectorateData} />
       <FestivalTeamSection cards={FestivalTeamData} />
       <VolunteersSection cards={VolunteersData} />
