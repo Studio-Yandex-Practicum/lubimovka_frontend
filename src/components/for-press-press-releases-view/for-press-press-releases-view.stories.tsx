5fca39d2b9c294b7a92eb83522cb69b3dec955d4
Anya Ch <annnacha@yandex.ru>
2021-11-14 22:19:55 +0300

    feature: верстка компонента с подгрузкой пресс-релиза

---
+++ src/components/for-press-press-releases-view/for-press-press-releases-view.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { ForPressPressReleasesView } from './for-press-press-releases-view';
+import mockData from './assets/mock-data.json';
+
+export default {
+  title: 'Components/ForPressPressReleasesView',
+  component: ForPressPressReleasesView,
+
+} as ComponentMeta<typeof ForPressPressReleasesView>;
+
+const Template: ComponentStory<typeof ForPressPressReleasesView> = (args) => {
+  return <ForPressPressReleasesView {...args} />;
+};
+
+export const Default = Template.bind({});
+
+Default.parameters = {
+  layout: 'fullscreen'
+};
+
+Default.args = {
+  data: mockData,
+};
---
+++ src/components/for-press-press-releases-view/for-press-press-releases-view.tsx
@@ -0,0 +1,87 @@
+import React, {FC, useState} from 'react';
+import cn from 'classnames/bind';
+
+import styles from './for-press-press-releases-view.module.css';
+
+import { Url } from 'shared/types';
+import { Droplist } from 'components/ui/droplist';
+import { Button } from 'components/ui/button';
+
+const cx = cn.bind(styles);
+
+export interface IForPressPressReleasesViewProps {
+data: {
+    cover: Url,
+    pressReleases: [{
+      year: number,
+      downloadLink: Url,
+      contents: [{
+        content_type: string;
+        content_item: Record<string, unknown>;
+      }],
+    }]
+   },
+}
+
+export const ForPressPressReleasesView: FC<IForPressPressReleasesViewProps> = ({ data }) => {
+
+  const pressReleaseYears = data.pressReleases.map((i)=> {return i.year;}).sort((a, b) => b - a);
+  const pressReleaseDefaultYear = pressReleaseYears[0];
+  const [pressReleaseYear, setPressReleaseYear] = useState<string[] | number>(pressReleaseDefaultYear);
+  const pressReleaseSelected = data.pressReleases.find(i => i.year == pressReleaseYear);
+
+  return (
+    <section className={cx('main')}>
+      <h3 className={cx('title')}>
+        Пресс-релизы
+      </h3>
+      <div className={cx('wrapper')}>
+        <p className={cx('droplistLabel')}>
+          Выберите год фестиваля
+        </p>
+        <Droplist
+          type='radio'
+          data={pressReleaseYears}
+          cb={(i: string[]) => {
+            setPressReleaseYear(Number(i[0]));
+          }}
+          className={cx('droplist')}
+        />
+        <Button
+          view='primary'
+          className={cx('button')}
+          align='center'
+          gap='11px'
+          size='s'
+          border='bottomLeft'
+          iconPlace='right'
+          icon='arrow-down'
+          label={`Скачать пресс-релиз ${pressReleaseYear} года в .pdf`}
+          isLink
+          href={pressReleaseSelected.downloadLink}
+        />
+        <img className={cx('cover')} src={data.cover}/>
+        <article className={cx('pressReleaseText')}>
+          {pressReleaseSelected.contents.map((item, idx) => {
+            switch (item.content_type) {
+            case 'preamble':
+              return(<h6 key={idx}>{item.content_item.preamble}</h6>);
+            case 'title':
+              return(<h4 key={idx}>{item.content_item.title}</h4>);
+            case 'list':
+              return(
+                <ul key={idx}>
+                  {Object.values(item.content_item).map((listItem: string, idx) => {
+                    return(<li key={idx}>{listItem}</li>);
+                  })
+                  }
+                </ul>);
+            case 'text':
+              return(<p key={idx}>{item.content_item.text}</p>);
+            }
+          })}
+        </article>
+      </div>
+    </section>
+  );
+};
