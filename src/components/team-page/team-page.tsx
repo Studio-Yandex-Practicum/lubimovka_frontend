230c553a18a235dc6e3a881e546ff6b03cdcdd51
Ирина Пупкова <pupkova.ia@yandex.ru>
2021-11-15 00:00:33 +0500

    refactor: использовать готовый компонент

--- src/components/team-page/team-page.tsx
+++ src/components/team-page/team-page.tsx
@@ -1,19 +1,50 @@
 import { FC } from 'react';
 
-import ArtFestTeamsData from './assets/mock-data-art.json';
-import VolunteersData from './assets/mock-data-volunteers.json';
-import SubmenuData from './assets/submenu-data.json';
-
+import { Menu } from 'components/ui/menu';
 import ArtDirectorateSection from './art-directorate/section/art-directorate-section';
 import FestivalTeamSection from './festival-team/festival-team-section';
 import VolunteersSection from './volunteers/section/volunteers-section';
-import Submenu from './submenu/submenu';
 
+import ArtFestTeamsData from './assets/mock-data-art.json';
+import VolunteersData from './assets/mock-data-volunteers.json';
+
+import styles from './team-page.module.css';
+import classNames from 'classnames';
+
+const cn = classNames;
+
+const demoItems = [
+  {
+    text: 'Что мы делаем',
+    href: '/what-we-do',
+  },
+  {
+    text: 'Организаторы',
+    href: '/team',
+  },
+  {
+    text: 'Попечители',
+    href: '/trustees',
+  },
+  {
+    text: 'Идеология',
+    href: '/ideology',
+  }
+];
 
 const TeamPage: FC = () => {
   return (
     <>
-      <Submenu submenu={SubmenuData}/>
+      <Menu type={ 'general-submenu' } className={cn(styles.submenu)}>
+        {demoItems.map(item => (
+          <Menu.Item
+            key={item.text}
+            href={item.href}
+          >
+            {item.text}
+          </Menu.Item>
+        ))}
+      </Menu>
       <ArtDirectorateSection cards={ArtFestTeamsData} />
       <FestivalTeamSection cards={ArtFestTeamsData} />
       <VolunteersSection cards={VolunteersData} />
