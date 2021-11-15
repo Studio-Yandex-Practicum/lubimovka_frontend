1ef89096a245aa08fe7f3a6c195264de6890418e
Tatiana S <tatyana-selyuk@yandex.com>
2021-11-15 23:05:59 +0200

    feat: Added Overview Component on Author page

--- src/components/author-page/overview/overview.tsx
+++ src/components/author-page/overview/overview.tsx
@@ -1,10 +1,15 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Button } from 'components/ui/button';
 import { Tag } from 'components/ui/tag';
 import { InfoLink } from 'components/ui/info-link';
 
 import cn from 'classnames';
 import styles from './overview.module.css';
+
+interface dataList {
+  id: string;
+  paragraph: string;
+}
 
 interface IAuthorOverview {
   data: {
@@ -13,20 +18,29 @@ interface IAuthorOverview {
     city: string,
     quote: string,
     description: string,
+    dataList: {
+      title: string;
+      list: dataList[];
+    }
   }
 }
 
+
 export const AuthorOverview: FC<IAuthorOverview> = ({data}) => {
 
-  const withPhoto = true;
+  const [isExpand, setExpand] = useState(true);
 
-  const isExpand = true;
+  const buttonClickHandlerOpen = () => setExpand(true);
+
+  const buttonClickHandlerClose = () => setExpand(false);
+
+  const withPhoto = true;
 
   return (
     <section className={ cn(styles.overview) }>
       <div className={ cn(withPhoto ? styles.personalInfo : styles.personalInfoNoPhoto) }>
         <div className={ cn(styles.button) }>
-          <Button size='s' iconPlace='right' icon='arrow-left' label='Библиотека' border='bottomRight' isLink={ true }/>
+          <Button size='s' iconPlace='right' icon='arrow-left' label='Библиотека' border='bottomRight' isLink={ true } />
         </div>
         { withPhoto &&
         <div className={ cn(styles.photoBox) }>
@@ -41,57 +55,60 @@ export const AuthorOverview: FC<IAuthorOverview> = ({data}) => {
       </div>
       <div className={ cn(styles.overviewInfo) }>
         <div className={ cn(styles.overviewBlock) }>
-          <p className={ cn(styles.overviewParagraph) }>{data.description}</p>
+          <p className={ cn(styles.overviewParagraph, isExpand ? styles.expandButton : styles.rollUpButton) }>{data.description}</p>
           { isExpand ?
-            <Button width='480px' size='s' iconPlace='right' icon='arrow-down' label='Полный текст' border='topLeft'/> :
-            <Button width='480px' size='s' iconPlace='right' icon='arrow-up' label='Свернуть' border='topLeft'/>}
-          {/*<button className={ cn(styles.expandButton) }>Полный текст</button>*/}
-          {/*<button className={ cn(styles.rollUpButton) }>Свернуть</button>*/}
+            <Button onClick={buttonClickHandlerClose} width='100%' size='s' iconPlace='right' icon='arrow-down' label='Полный текст' border='topLeft' /> :
+            <Button onClick={buttonClickHandlerOpen} width='100%' size='s' iconPlace='right' icon='arrow-up' label='Свернуть' border='topLeft' />
+          }
+          <div className={ cn(styles.overviewBlockAuthorInfo) }>
+            {data.dataList.list.map((item) =>
+              <div className={ cn(styles.overviewLinkHeading) } key={item.id}>
+                <InfoLink
+                  label={item.paragraph}
+                  icon='arrow-right'
+                  iconPlace='right'
+                  size='xl'
+                  border='borderTop'
+                  iconClassName={ cn(styles.link) }
+                />
+              </div>
+            )}
+          </div>
         </div>
         <div className={ cn(styles.overviewSet) }>
           <div className={ cn(styles.overviewTagsBlock) }>
             <h2 className={ cn(styles.overviewTagsHeading) }>Достижения</h2>
             <div className={ cn(styles.tag) }>
-              <Tag
-                label='шорт-лист'
-                selected={ false }
-              />
+              <Tag label='шорт-лист' selected={false}/>
+              <Tag label='fringe-программа' selected={false}/>
+              <Tag label='внеконкурсная программа' selected={false}/>
             </div>
-            <div className={ cn(styles.tag) }>
-              <Tag
-                label='внеконкурсная программа'
-                selected={ false }
+          </div>
+
+          <div className={ cn(styles.overviewSocialWrapper) }>
+            <h2 className={ cn(styles.overviewSocialLinkHeading) }>Социальные сети</h2>
+            <div className={ cn(styles.overviewSocialLinkBlock) }>
+              <InfoLink
+                isOutsideLink={true}
+                href='https://www.facebook.com/festival.lubimovka'
+                label='fb'
+                icon='arrow-right'
+                iconPlace='left'
+                size='s'
+                border='borderBottomLeft'
               />
-            </div>
-            <div className={ cn(styles.tag) }>
-              <Tag
-                label='fringe-программа'
-                selected={ false }
+              <InfoLink
+                isOutsideLink={true}
+                href='https://www.facebook.com/festival.lubimovka'
+                label='vk'
+                icon='arrow-right'
+                iconPlace='left'
+                size='s'
+                border='borderBottomLeft'
               />
             </div>
           </div>
-          <div>
-            <h2 className={ cn(styles.overviewTagsHeading) }>Социальные сети</h2>
-            <InfoLink
-              isOutsideLink={true}
-              href='https://www.facebook.com/festival.lubimovka'
-              label='fb'
-              icon='arrow-right'
-              iconPlace='left'
-              size='s'
-              border='borderBottomLeft'
-            />
-            <InfoLink
-              isOutsideLink={true}
-              href='https://www.facebook.com/festival.lubimovka'
-              label='vk'
-              icon='arrow-right'
-              iconPlace='left'
-              size='s'
-              border='borderBottomLeft'
-            />
-          </div>
-          <div>
+          <div className={ cn(styles.overviewSocialWrapper) }>
             <p className={ cn(styles.email) }>E-mail для связи</p>
             <InfoLink
               isOutsideLink={true}
