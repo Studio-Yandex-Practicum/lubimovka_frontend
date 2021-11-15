31e620179931e1c0e55fd5ff854c00b3f9e343d9
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-11-15 12:52:40 -0600

    fix: убран проп type компонента Video

--- src/components/video/video.tsx
+++ src/components/video/video.tsx
@@ -4,15 +4,14 @@ import styles from './video.module.css';
 import { Url } from 'shared/types';
 
 interface IVideoProps {
-  type: 'performance';
   link: Url,
 }
 const cx = classNames.bind(styles);
 
 export const Video = (props: IVideoProps): JSX.Element => {
-  const { type, link } = props;
+  const { link } = props;
 
   return (
-    <iframe className={cx('video', type)} src={link} allowFullScreen />
+    <iframe className={cx('video')} src={link} allowFullScreen />
   );
 };
