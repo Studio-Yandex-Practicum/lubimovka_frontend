4c777e12aef923ae68f5f3a616581ff6d2000717
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-11-17 16:39:41 +0300

    chore: обновление тайпингов API

---
+++ src/api-typings/models/PaginatedProjectListList.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ProjectList } from './ProjectList';
+
+export type PaginatedProjectListList = {
+    count?: number;
+    next?: string | null;
+    previous?: string | null;
+    results?: Array<ProjectList>;
+}
