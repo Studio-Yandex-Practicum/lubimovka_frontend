c40bf8fc6910d949d9b09eafb40a75d22c047c88
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-11-19 23:21:53 +0300

    refactor: рефакторинг общей раскладки

---
+++ src/components/page/overlay-menu/page-overlay-menu.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames/bind';
+
+import styles from './page-overlay-menu.module.css';
+
+const cx = classNames.bind(styles);
+
+interface IPageOverlayMenuProps {
+  isOpen: boolean;
+  children: React.ReactNode;
+}
+
+export const PageOverlayMenu = (props: IPageOverlayMenuProps): JSX.Element => {
+  const { isOpen, children } = props;
+
+  return (
+    <div className={cx('menu', { visible: isOpen })}>
+      {children}
+    </div>
+  );
+};
