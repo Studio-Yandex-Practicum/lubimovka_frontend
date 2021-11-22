3e9a775570b7c5ca90f5e8083dd78ad372f3831d
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-11-22 12:19:15 -0500

    feat: добавление валидации формы страницы /contacts

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -11,10 +12,61 @@ import Form from 'components/ui/form/form';
 import TextInput from 'components/ui/text-input/text-input';
 import TextArea from 'components/ui/text-area';
 import { Button } from 'components/ui/button';
+import { validEmailRegexp } from 'shared/regexps';
 
 import playScript from '/public/images/contacts/play-script.jpg';
 
 const Contacts: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [name, setName] = useState('');
+  const [nameWasChanged, setNameWasChanged] = useState(false);
+  const [email, setEmail] = useState('');
+  const [emailWasChanged, setEmailWasChanged] = useState(false);
+  const [question, setQuestion] = useState('');
+  const [questionWasChanged, setQuestionWasChanged] = useState(false);
+
+  const getNameError = () => {
+    if (name.length < 2) {
+      return 'Имя должно содержать минимум 2 символа';
+    }
+
+    return;
+  };
+
+  const getEmailError = () => {
+    if (!email.length) {
+      return 'Поле E-mail обязательно для заполнения';
+    }
+
+    if (!validEmailRegexp.test(email)) {
+      return 'Неверный формат адреса электронной почты';
+    }
+
+    return;
+  };
+
+  const getQuestionError = () => {
+    if (!question.length) {
+      return 'Поле обязательно для заполнения';
+    }
+
+    return;
+  };
+
+  const handleNameChange = (value: string) => {
+    setNameWasChanged(true);
+    setName(value);
+  };
+
+  const handleEmailChange = (value: string) => {
+    setEmailWasChanged(true);
+    setEmail(value);
+  };
+
+  const handleQuestionChange = (value: string) => {
+    setQuestionWasChanged(true);
+    setQuestion(value);
+  };
+
   return (
     <AppLayout>
       <ContactsLayout>
@@ -26,18 +78,26 @@ const Contacts: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServ
             <Form aria-labelledby='contact'>
               <Form.Field>
                 <TextInput
+                  value={name}
                   placeholder="Ваше имя"
+                  errorText={nameWasChanged ? getNameError() : undefined}
+                  onChange={handleNameChange}
                 />
               </Form.Field>
               <Form.Field>
                 <TextInput
+                  value={email}
                   placeholder="E-mail для ответа"
+                  errorText={emailWasChanged ? getEmailError() : undefined}
+                  onChange={handleEmailChange}
                 />
               </Form.Field>
               <Form.Field>
                 <TextArea
-                  ariaLabel=""
+                  value={question}
                   placeholder="Текст сообщения"
+                  errorText={questionWasChanged ? getQuestionError() : undefined}
+                  onChange={handleQuestionChange}
                   rows={4}
                 />
               </Form.Field>
