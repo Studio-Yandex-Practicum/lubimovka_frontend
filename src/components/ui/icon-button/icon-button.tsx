ace34dbbdb35bd71cff5cc51be77cea17240d538
Ilya Borodulin <borodulinid@gmail.com>
2021-11-23 23:50:56 +0300

    feat: добавлен компонент IconButton

---
+++ src/components/ui/icon-button/icon-button.tsx
@@ -0,0 +1,36 @@
+import { ButtonHTMLAttributes } from 'react';
+import classNames from 'classnames/bind';
+
+import styles from './icon-button.module.css';
+
+interface IIconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  type: 'button' | 'submit';
+  view: 'ghost' | 'light';
+  ariaLabel: string;
+  icon: React.ReactNode;
+}
+
+const cx = classNames.bind(styles);
+
+export const IconButton = (props: IIconButtonProps): JSX.Element => {
+  const {
+    className = '',
+    type,
+    view = 'ghost',
+    ariaLabel,
+    icon,
+    ...restProps
+  } = props;
+
+  return(
+    <button
+      className={cx(className, 'button', [view])}
+      type={type}
+      aria-label={ariaLabel}
+      {...restProps}
+    >
+      {icon}
+    </button>
+  );
+};
