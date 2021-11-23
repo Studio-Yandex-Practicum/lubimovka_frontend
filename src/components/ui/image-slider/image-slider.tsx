e35158463acff17aed40525ee89ef79b66e35825
Ilya Borodulin <borodulinid@gmail.com>
2021-11-23 16:36:17 +0300

    refactor: SliderButton стили вынесены в псевдокласс

--- src/components/ui/image-slider/image-slider.tsx
+++ src/components/ui/image-slider/image-slider.tsx
@@ -51,6 +51,7 @@ export const ImageSlider = (props: IImageSliderProps): JSX.Element => {
                 icon="arrow-left"
                 type='navigation'
                 view='article'
+                direction='left'
                 onClick={slider.prev}
               />
             </div>
@@ -59,6 +60,7 @@ export const ImageSlider = (props: IImageSliderProps): JSX.Element => {
                 icon="arrow-right"
                 type='navigation'
                 view='article'
+                direction='right'
                 onClick={slider.next}
               />
             </div>
