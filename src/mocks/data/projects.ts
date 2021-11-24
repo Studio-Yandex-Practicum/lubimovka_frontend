c55303ccec623f6689713967175b441c679473e6
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-11-24 07:53:13 +0300

    chore: улучшение моков проектов

--- src/mocks/data/projects.ts
+++ src/mocks/data/projects.ts
@@ -4,37 +4,37 @@ const projects: ProjectList[] = [
   {
     id: 1,
     title: 'Любимовка. Ещё',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor pulvinar gravida. Proin vel dui non nisl interdum congue sed non ligula. Curabitur mattis quam quis sodales eleifend. Pellentesque porta ex sit amet nisl commodo, laoreet pulvinar augue tempor.',
+    description: 'Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки. Можно слушать, обсуждать и даже участвовать.',
     image: 'https://source.unsplash.com/random',
   },
   {
     id: 2,
     title: 'Любимовка. Пьесы',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor pulvinar gravida. Proin vel dui non nisl interdum congue sed non ligula. Curabitur mattis quam quis sodales eleifend. Pellentesque porta ex sit amet nisl commodo, laoreet pulvinar augue tempor.',
+    description: 'Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки. Можно слушать, обсуждать и даже участвовать.',
     image: 'https://source.unsplash.com/random',
   },
   {
     id: 3,
     title: 'Lark + Любимовка',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor pulvinar gravida. Proin vel dui non nisl interdum congue sed non ligula. Curabitur mattis quam quis sodales eleifend. Pellentesque porta ex sit amet nisl commodo, laoreet pulvinar augue tempor.',
+    description: 'Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки. Можно слушать, обсуждать и даже участвовать.',
     image: 'https://source.unsplash.com/random',
   },
   {
     id: 4,
     title: 'Практика постдраматурга',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor pulvinar gravida. Proin vel dui non nisl interdum congue sed non ligula. Curabitur mattis quam quis sodales eleifend. Pellentesque porta ex sit amet nisl commodo, laoreet pulvinar augue tempor.',
+    description: 'Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки. Можно слушать, обсуждать и даже участвовать.',
     image: 'https://source.unsplash.com/random',
   },
   {
     id: 5,
     title: 'Спектакли',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor pulvinar gravida. Proin vel dui non nisl interdum congue sed non ligula. Curabitur mattis quam quis sodales eleifend. Pellentesque porta ex sit amet nisl commodo, laoreet pulvinar augue tempor.',
+    description: 'Межсезонные читки и обсуждение пьес из списка отмеченных отборщиками Любимовки. Можно слушать, обсуждать и даже участвовать.',
     image: 'https://source.unsplash.com/random',
   },
   {
     id: 6,
     title: 'Эхо Любимовки',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempor pulvinar gravida. Proin vel dui non nisl interdum congue sed non ligula. Curabitur mattis quam quis sodales eleifend. Pellentesque porta ex sit amet nisl commodo, laoreet pulvinar augue tempor.',
+    description: 'Читки пьес основной программы Любимовки в разных регионах России и Зарубежья в постфестивальный период.',
     image: 'https://source.unsplash.com/random',
   },
 ];
