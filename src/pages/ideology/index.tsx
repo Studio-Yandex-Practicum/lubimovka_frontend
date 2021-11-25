13760a363e4177892ef457cedb622f1bcbc02e06
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-11-25 19:27:55 +0400

    feat: добавить AboutUsMenu на страницу ideology

--- src/pages/ideology/index.tsx
+++ src/pages/ideology/index.tsx
@@ -3,19 +3,25 @@ import Head from 'next/head';
 
 import TextSection from 'components/ideology-section';
 import AppLayout from 'components/app-layout';
+import { AboutUsMenu } from 'components/what-we-do-page/about-us-menu';
 
 import textData from './assets/mock-data.json';
+
+import style from './index.module.css';
 
 const Ideology: NextPage = () => (
   <AppLayout>
     <Head>
       <title>Идеология</title>
     </Head>
-    <>
+    <div className={style.content}>
+      <div className={style.menu}>
+        <AboutUsMenu/>
+      </div>
       {textData.map((el) => (
         <TextSection key={el.id} data={el}/>
       ))}
-    </>
+    </div>
   </AppLayout>
 );
 
