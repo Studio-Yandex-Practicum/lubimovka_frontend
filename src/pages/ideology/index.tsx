289703736985de204a7f9f46ccd1ddd3f55fd3c4
Evgeniia Zubova <ev.zu8ova@yandex.ru>
2021-11-25 20:48:37 +0400

    fix: вынести компонент страницы ideology

--- src/pages/ideology/index.tsx
+++ src/pages/ideology/index.tsx
@@ -1,27 +1,17 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 
-import TextSection from 'components/ideology-section';
 import AppLayout from 'components/app-layout';
-import { AboutUsMenu } from 'components/what-we-do-page/about-us-menu';
+import IdeologyPage from 'components/ideology-page';
 
 import textData from './assets/mock-data.json';
-
-import style from './index.module.css';
 
 const Ideology: NextPage = () => (
   <AppLayout>
     <Head>
       <title>Идеология</title>
     </Head>
-    <div className={style.content}>
-      <div className={style.menu}>
-        <AboutUsMenu/>
-      </div>
-      {textData.map((el) => (
-        <TextSection key={el.id} data={el}/>
-      ))}
-    </div>
+    <IdeologyPage data={textData}/>
   </AppLayout>
 );
 
