9ec465dbeff9cdab2df084e93e29515688770e20
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2021-11-27 18:18:37 +0300

    fix: исправление импортов модулей

--- src/components/section/section.stories.tsx
+++ src/components/section/section.stories.tsx
@@ -4,7 +4,7 @@ import { Section } from 'components/section';
 import { BasicPlayCardList } from 'components/ui/basic-play-card-list';
 
 import { Default as DefaultBasicPlayCardList } from 'components/ui/basic-play-card-list/basic-play-card-list.stories';
-import { Default as DefaultPersonCardList } from 'components/ui/person-card/list/person-card-list.stories';
+import { Default as DefaultPersonCardList } from 'components/person-card-list/person-card-list.stories';
 
 export default {
   title: 'Components/Section',
