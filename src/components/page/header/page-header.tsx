4e58cd77cdbf3f98b780a6952217e6eee58d53ce
Irina Naumchik <irina.naumchik2012@yandex.ru>
2021-11-28 12:49:38 -0600

    feat: Добавление необходимых пропов для отрисовки шапки с крупным лого

--- src/components/page/header/page-header.tsx
+++ src/components/page/header/page-header.tsx
@@ -1,15 +1,23 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './page-header.module.css';
 
+interface IPageHeaderProps {
+  expanded?: boolean;
+  children: ReactNode,
+}
+
 const cx = classNames.bind(styles);
 
-export const PageHeader: FC = (props) => {
-  const { children } = props;
+export const PageHeader: FC<IPageHeaderProps> = (props) => {
+  const {
+    expanded,
+    children,
+  } = props;
 
   return (
-    <header className={cx('header')}>
+    <header className={cx('header', { expanded })}>
       {children}
     </header>
   );
