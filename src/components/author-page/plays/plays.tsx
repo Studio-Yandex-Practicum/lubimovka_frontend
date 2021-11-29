2540b2ca08fe5d6b024d17232dae5c7905ace8b3
Tatiana S <tatyana-selyuk@yandex.com>
2021-11-29 20:42:56 +0200

    fix: Plays Component

--- src/components/author-page/plays/plays.tsx
+++ src/components/author-page/plays/plays.tsx
@@ -6,27 +6,29 @@ import { BasicPlayCard } from 'components/ui/basic-play-card';
 import styles from './plays.module.css';
 
 interface PlaysData {
-  title: string
-  city: string
-  year: string
+  title: string,
+  city: string,
+  year: string,
   linkView: string,
-  linkDownload: string
-  author: AuthorData
+  linkDownload: string,
+  author: AuthorData,
 }
+
 interface AuthorData {
-  id: number
-  name: string
+  id: number,
+  name: string,
 }
+
 interface IAuthorPlays {
   data: {
-    plays: PlaysData[]
-    title: string
+    plays: PlaysData[],
+    title: string,
   }
 }
 
 export const AuthorPlays: FC<IAuthorPlays> = ({ data }) => {
   return (
-    <section className={cn(styles.plays)}>
+    <section className={cn(styles.playsContainer)}>
       {data.plays.map((item) => (
         <BasicPlayCard play={item} author={item.author} key={item.author.id}/>
       ))}
