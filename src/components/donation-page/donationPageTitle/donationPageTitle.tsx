d4cb8af430acc774a8e1cde2e212e7e82d85a540
Yaroslav Bashmakov <puer.robustus@gmail.com>
2021-11-30 18:38:34 +0300

    feat(donation-page): добавить QR-код на страницу донатов

--- src/components/donation-page/donationPageTitle/donationPageTitle.tsx
+++ src/components/donation-page/donationPageTitle/donationPageTitle.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import cn from 'classnames/bind';
 
 import { Icon } from 'components/ui/icon';
 
+import qrcode from '../../../../public/images/donation/lubimovka-qrcode.jpeg';
 import styles from './donationPageTitle.module.css';
 
 const cx = cn.bind(styles);
@@ -37,6 +40,23 @@ export const DonationPageTitle: FC<IDonationPageTitleProps> = (props) => {
           name="goody_container_form"
           frameBorder="no"
         />
+        <div className={cx('qrcodeContainer')}>
+          <Link href="https://qr.nspk.ru/AS10004LII9QK8KP8ADRSSI2TIAV1NNF?type=01&bank=100000000007&crc=D63D">
+            <a>
+              <div className={cx('qrcode')}>
+                <Image
+                  src={qrcode}
+                  alt='QR-code для отправки доната'
+                  layout='fill'
+                  objectFit='cover'
+                />
+              </div>
+            </a>
+          </Link>
+          <p className={cx('qrcodeAdvise')}>
+            Поддержать фестиваль можно ещё проще! Откройте приложение своего банка, выберите функцию &quot;Оплата по QR-коду&quot; и отсканируйте этот код. Или просто нажмите на QR-код, приложение вашего банка откроется само.
+          </p>
+        </div>
       </div>
     </section>
   );
