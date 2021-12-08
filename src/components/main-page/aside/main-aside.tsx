47dd3d0d96527f8058ca4114258a84d4dc250c69
Rustam <Rustamaaaa@bk.ru>
2021-12-08 17:58:05 +0300

    feat/Добавил аналог position: sticky

--- src/components/main-page/aside/main-aside.tsx
+++ src/components/main-page/aside/main-aside.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import cn from 'classnames/bind';
 
 import { Button } from 'components/ui/button';
@@ -11,8 +11,31 @@ const cx = cn.bind(styles);
 
 export const MainAside: FC = () => {
   const { title, buttonText, buttonLink, blogData } = data.aside;
+
+  const asideRef = useRef(null);
+
+  const scrollHandler = () => {
+    const heightWindow = document.documentElement;
+    if (window.scrollY < 61 && asideRef.current) {
+      asideRef.current.classList.remove(cx('header'));
+    }
+    if (window.scrollY > 61 && asideRef.current) {
+      asideRef.current.classList.add(cx('header'));
+    }
+    if (asideRef.current && heightWindow.scrollHeight - heightWindow.scrollTop < heightWindow.clientHeight + 390) {
+      asideRef.current.classList.add(cx('footer'));
+    }
+    if (asideRef.current && heightWindow.scrollHeight - heightWindow.scrollTop > heightWindow.clientHeight + 390) {
+      asideRef.current.classList.remove(cx('footer'));
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', scrollHandler);
+  }, []);
+
   return (
-    <aside className={cx('aside')}>
+    <aside className={cx('aside')} ref={asideRef}>
       <div className={cx('heading')}>
         <h2 className={cx('title')}>{title}</h2>
         <div className={cx('buttonContainer')}>
