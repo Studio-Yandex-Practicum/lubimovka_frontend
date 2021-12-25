268e2ce0353d257f4d62995602a4a1e802210a13
Anya Ch <annnacha@yandex.ru>
2021-12-25 20:05:41 +0300

    feat: рендеринг одновременно и описания, и кредитсов в AnnouncedPlayCard

--- src/components/ui/announced-play-card/announced-play-card.stories.tsx
+++ src/components/ui/announced-play-card/announced-play-card.stories.tsx
@@ -28,6 +28,21 @@ PerformancePaid.args = {
   paid: true,
 };
 
+export const PerformancePaidWithDescription = Template.bind({});
+PerformancePaidWithDescription.args = {
+  isPerformance: true,
+  id: 1,
+  date:'2021-11-13T17:00:00.000Z',
+  title: 'МАМА',
+  dramatists: ['Ольга Казакова', 'Антон Чехов'],
+  directors: ['Катя Ганюшина'],
+  description: 'Гости расскажут о своём творческом и организационном опыте и вдохновят аудиторию преодолевать любые границы.',
+  buttonLink: 'https://lubimovka.timepad.ru/event/1746579/',
+  imageUrl: 'https://img05.rl0.ru/afisha/1808x1016q65i/s2.afisha.ru/mediastorage/5e/c5/541412eb0ea14286bad43d20c55e.jpg',
+  projectText: 'читка проекта Любимовка.Ещё',
+  paid: true,
+};
+
 export const PerformanceNoTickets = Template.bind({});
 PerformanceNoTickets.args = {
   isPerformance: true,
@@ -76,6 +91,20 @@ ReadingNoCredits.args = {
   title: 'Что я узнал о творчестве благодаря драматургам',
   dramatists: [],
   directors: [],
+  description: 'Гости расскажут о своём творческом и организационном опыте и вдохновят аудиторию преодолевать любые границы.',
+  buttonLink: 'https://lubimovka.timepad.ru/event/1746579/',
+  projectText: 'читка проекта Любимовка.Ещё',
+  paid: false,
+};
+
+export const ReadingWithCredits = Template.bind({});
+ReadingWithCredits.args = {
+  isPerformance: true,
+  id: 1,
+  date:'2021-11-13T17:00:00.000Z',
+  title: 'Коммуналка на Петроградской',
+  dramatists: ['Ольга Казакова', 'Антон Чехов'],
+  directors: ['Катя Ганюшина'],
   description: 'Гости расскажут о своём творческом и организационном опыте и вдохновят аудиторию преодолевать любые границы.',
   buttonLink: 'https://lubimovka.timepad.ru/event/1746579/',
   projectText: 'читка проекта Любимовка.Ещё',
