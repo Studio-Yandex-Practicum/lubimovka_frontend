cb12e100da7bb8f1c0688fdf75c241add7879c3f
Rustam <Rustamaaaa@bk.ru>
2022-01-12 16:08:34 +0300

    feat/Добавил стили

--- src/components/main-page/first-screen/first-screen.tsx
+++ src/components/main-page/first-screen/first-screen.tsx
@@ -10,27 +10,25 @@ const cx = classNames.bind(styles);
 
 export const FirstScreen: FC = () => {
   return (
-    <>
-      <section className={cx('firstScreen')}>
-        <div className={cx('container')}>
-          <h1 className={cx('title')}>
-				Открыт приём пьес на фестиваль {new Date().getFullYear()} года
-          </h1>
-          <div className={cx('wrapper')}>
-            <Button
-              label={'ПОДАТЬ ПЬЕСУ'}
-              isLink
-              href='#'
-              size='l'
-              border='full'
-              icon="arrow-right"
-              iconPlace='right'
-              width="100%"
-              className={cx('button')}
-            />
-          </div>
+    <section className={cx('firstScreen')}>
+      <div className={cx('container')}>
+        <h1 className={cx('title')}>
+          Открыт приём пьес на фестиваль {new Date().getFullYear()} года
+        </h1>
+        <div className={cx('wrapper')}>
+          <Button
+            label={'ПОДАТЬ ПЬЕСУ'}
+            isLink
+            href='#'
+            size='l'
+            border='full'
+            icon="arrow-right"
+            iconPlace='right'
+            width="100%"
+            className={cx('button')}
+          />
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
