a65f36f9ffb8f5105f4c2c23bf2263f8dc236663
Rustam <Rustamaaaa@bk.ru>
2022-01-14 15:49:57 +0300

    feat/Добавил картинку

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable import/no-unresolved */
 import { NextPage } from 'next';
 import Head from 'next/head';
 import cn from 'classnames/bind';
+import Image from 'next/image';
 
 import { MainTitle } from 'components/main-page/title';
 import { MainEvents } from 'components/main-page/events';
@@ -19,15 +19,25 @@ import mainEventsData from 'components/main-page/assets/main-events.json';
 import mainShortListData from 'components/main-page/assets/main-short-list-data.json';
 import mainArchiveData from 'components/main-page/assets/main-archive-data.json';
 import mainPlatformsData from 'components/main-page/assets/main-platforms-data.json';
+import { useMediaQuery } from 'shared/hooks/use-media-query';
+import * as breakpoints from 'shared/breakpoints.js';
 import styles from './index.module.css';
 
 const cx = cn.bind(styles);
 
 const MainPage: NextPage = () => {
   const { title, events, aside, banners, platforms, partners, archive, shortList, metaTitle } = data;
+  const isMobile = useMediaQuery(`(max-width: ${breakpoints['tablet-portrait']})`);
   return (
     <>
-      <AppLayout hiddenPartners>
+      <AppLayout hiddenPartners screenImg={true && <div className={cx('wrapper')}>
+        <Image 
+          alt='screen' 
+          src={isMobile ? '/images/main/screen-mobile.jpg' : '/images/main/screen.jpg'}
+          layout='fill'
+          objectFit='fill'
+        />
+      </div>}>
         <>
           <Head>
             <title>{metaTitle}</title>
