578f0a2f81c6e27d9ddf6c9acb3f93c49d7ed9fa
Rustam <Rustamaaaa@bk.ru>
2022-01-18 16:29:06 +0300

    fix/Изменил реализоцию компонента main-banners

--- src/components/main-page/banners/main-banners.tsx
+++ src/components/main-page/banners/main-banners.tsx
@@ -1,55 +1,58 @@
-import React, { FC, useEffect, useState, useCallback } from 'react';
+import React, { FC } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import cn from 'classnames/bind';
-
 import { Banner, MainBanners as IMainBanners } from 'api-typings';
-import { MainBannerItem } from './banner';
+
+import { Button } from 'components/ui/button';
 
 import styles from './main-banners.module.css';
 
 const cx = cn.bind(styles);
 
-function debounce(cb: () => void, delay: number) {
-  let timer: ReturnType<typeof setTimeout>;
-  return function() {
-    clearTimeout(timer);
-    timer = setTimeout(cb, delay || 0);
-  };
-}
-
-export const MainBanners: FC<IMainBanners> = ({ items }) => {
-  const [scrollHandlers, setScrollHandler] = useState<(() => void)[]>([]);
-
-  useEffect(() => {
-    const handlerDebounced = debounce(() => scrollHandlers.map((item: () => void) => item()), 10);
-    if (scrollHandlers.length) {
-      window.addEventListener('scroll', handlerDebounced);
-    }
-    return () => { window.removeEventListener('scroll', handlerDebounced);};
-  }, [scrollHandlers]);
-
-  const cb = useCallback((handlerDebounced: () => void) => {
-    setScrollHandler(state => [...state, handlerDebounced]);
-  }, [setScrollHandler]);
-
-  return (
-    <section className={cx('banners')}>
-      <ul className={cx('list')}>
-        {
-          items.map((item: Banner) => (
-            <li className={cx('item')} key={item.id}>
-              <MainBannerItem 
-                cb={cb} 
-                title={item.title} 
-                desc={item.description} 
-                buttonText={item.button}
-                buttonUrl={item.url}
-                imgAlt={item.title}
-                image={item.image}
-              />
+export const MainBanners: FC<IMainBanners> = ({ items }) => (
+  <section className={cx('banners')}>
+    <ul className={cx('list')}>
+      {
+        items.map((item: Banner) => (
+          <>
+            <li className={cx('item')}>
+              <h3 className={cx('title')}>
+                {item.title}
+              </h3>
+              <aside className={cx('container')}>
+                <div className={cx('content')}>
+                  <p className={cx('desc')}>
+                    {item.description}
+                  </p>
+                  <Button 
+                    label={item.button}
+                    iconPlace='left' 
+                    icon='arrow-right' 
+                    gap='4px'
+                    border='bottomLeft'
+                    isLink={true}
+                    href={item.url}
+                    className={cx('button')}
+                  />
+                </div>
+                <Link href={item.url}>
+                  <a className={cx('link')} target='_blank'>
+                    <Image 
+                      src={item.image}
+                      alt={item.title}
+                      width={486}
+                      height={228}
+                      layout='responsive'
+                      objectFit='cover'
+                    />
+                  </a>
+                </Link>
+              </aside>
             </li>
-          ))
-        }
-      </ul>
-    </section>
-  );
-};
+          </>
+        ))
+      }
+    </ul>
+  </section>
+);
