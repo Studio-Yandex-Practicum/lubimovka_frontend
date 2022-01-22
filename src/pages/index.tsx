76aa4c1ed0fe3e996425ba735de03d83b651ddc4
Rustam <Rustamaaaa@bk.ru>
2022-01-22 16:15:51 +0300

    feat/Реализовал прокрутку страницы с помощью margin-top

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import { NextPage, InferGetStaticPropsType, GetStaticProps } from 'next';
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Head from 'next/head';
 import cn from 'classnames/bind';
-import Image from 'next/image';
 import { Main, Partner } from 'api-typings';
 
 import { fetcher } from 'shared/fetcher';
-import { useMediaQuery } from 'shared/hooks/use-media-query';
-import breakpoints from 'shared/breakpoints';
 import { MainTitle } from 'components/main-page/title';
 import { MainEvents } from 'components/main-page/events';
 import { MainAside } from 'components/main-page/aside';
@@ -28,49 +25,36 @@ const MainPage: NextPage = ({ data = main, partners }: InferGetStaticPropsType<t
   const { first_screen, afisha, blog, news, banners, places, video_archive, short_list } = data;
 
   const [displayFirstScreen, setDisplayFirstScreen] = useState(true);
-  const isMobile = useMediaQuery(`(max-width: ${breakpoints['tablet-portrait']})`);
+  const [delay, setDelay] = useState(false);
 
-  const wrapperImageRef = useRef(null);
-  const firstScreenRef = useRef(null);
-
-  const handlerScroll = useCallback(() => {
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-    const top = firstScreenRef.current.clientHeight + wrapperImageRef.current.clientHeight + 72;
-    const delay = top / 1.8;
-    window.scrollTo({
-      top: top,
-      behavior: 'smooth'
-    });
-    document.body.style.paddingRight = scrollbarWidth + 'px';
-    document.body.style.overflowY = 'hidden';
+  const hideFirstScreen = useCallback((delay: number) => {
     setTimeout(() => {
+      setDelay(false);
       setDisplayFirstScreen(false);
     }, delay);
-    setTimeout(() => {
-      document.body.style.overflowY = '';
-      document.body.style.paddingRight = '';
-    }, delay + 10);
-  }, [setDisplayFirstScreen]);
+  }, [displayFirstScreen]);
+
+  const handlerScroll = useCallback(() => {
+    setDelay(true);
+    hideFirstScreen(1000);
+  }, [delay]);
 
   useEffect(() => {
-    window.addEventListener('scroll', handlerScroll, { once: true });
+    window.addEventListener('scroll', handlerScroll);
     if (displayFirstScreen === false) {
       window.removeEventListener('scroll', handlerScroll);
     }
-    // console.log(window.pageYOffset);
+    return () => {
+      window.removeEventListener('scroll', handlerScroll);
+    };
+  }), [];
+
+  useEffect(() => {
+    // Отключаю скролл, при перезагрузке страницы
     if (window.pageYOffset !== 0) {
       window.removeEventListener('scroll', handlerScroll);
       setDisplayFirstScreen(false);
     }
-  }), [displayFirstScreen];
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      if (wrapperImageRef.current) {
-        firstScreenRef.current.style.height = '100vh';
-        firstScreenRef.current.style.height = firstScreenRef.current.clientHeight - wrapperImageRef.current.clientHeight - 147 + 'px';
-      }
-    });
   }, []);
 
   function notEmpty<T>(items: T[]) {
@@ -82,42 +66,38 @@ const MainPage: NextPage = ({ data = main, partners }: InferGetStaticPropsType<t
   }
 
   return (
-    <AppLayout hiddenPartners expandedHeader={displayFirstScreen}
-      screenImg={first_screen && notEmptyKey(first_screen) &&
-       displayFirstScreen && <div className={cx('wrapperImage')} ref={wrapperImageRef}>
-        <Image 
-          alt="screen" 
-          src={isMobile ? '/images/main/screen-mobile.jpg' : '/images/main/screen.jpg'}
-          layout="fill"
-          objectFit="cover"
-        />
-      </div>}>
-      <>
-        <Head>
-          <title>Главная</title>
-        </Head>
-        <main className={cx('main')}>
-          {first_screen && notEmptyKey(first_screen) && displayFirstScreen && <div className={cx('wrapperScreen')} ref={firstScreenRef}>
-            <MainFirstScreen/>
-          </div>}
-          {news ? <MainAside type="news" {...news}/> : <MainAside type="blog" {...blog}/>}
-          {afisha && notEmptyKey(afisha) &&
-          <div className={cx('wrapper')}>
-            <MainTitle
-              title={afisha.title}
-              button_label={afisha.button_label}
-              description={afisha.description}
-            />
-          </div>}
-          {afisha && notEmpty(afisha.items) && <MainEvents {...afisha}/>}
-          {banners && notEmpty(banners.items) && <MainBanners {...banners}/>}
-          {short_list && notEmpty(short_list.items) && <MainShortList {...short_list}/>}
-          {places && notEmpty(places.items) && <MainPlatforms {...places}/>}
-          {video_archive && <MainArchive {...video_archive}/>}
-          {partners && notEmptyKey(partners) && <MainPartners {...partners}/>}
-        </main>
-      </>
-    </AppLayout>
+    <div className={cx({ 'marginTop': delay })}>
+      <AppLayout 
+        hiddenPartners 
+        expandedHeader={displayFirstScreen}
+        screenImg={first_screen && notEmptyKey(first_screen) &&
+        displayFirstScreen && <div className={cx('background')}/>}
+      >
+        <>
+          <Head>
+            <title>Главная. Любимовка</title>
+          </Head>
+          <main className={cx('main')}>
+            {first_screen && notEmptyKey(first_screen) && displayFirstScreen && <MainFirstScreen/>}
+            {news ? <MainAside type="news" {...news}/> : <MainAside type="blog" {...blog}/>}
+            {afisha && notEmptyKey(afisha) &&
+            <div className={cx('wrapper')}>
+              <MainTitle
+                title={afisha.title}
+                button_label={afisha.button_label}
+                description={afisha.description}
+              />
+            </div>}
+            {afisha && notEmpty(afisha.items) && <MainEvents {...afisha}/>}
+            {banners && notEmpty(banners.items) && <MainBanners {...banners}/>}
+            {short_list && notEmpty(short_list.items) && <MainShortList {...short_list}/>}
+            {places && notEmpty(places.items) && <MainPlatforms {...places}/>}
+            {video_archive && <MainArchive {...video_archive}/>}
+            {partners && notEmptyKey(partners) && <MainPartners {...partners}/>}
+          </main>
+        </>
+      </AppLayout>
+    </div>
   );
 };
 
