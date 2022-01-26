f2e4a3c534935c425609c43228e640a2996f4770
Rustam <Rustamaaaa@bk.ru>
2022-01-26 17:22:25 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,6 @@ import styles from './index.module.css';
 const cx = cn.bind(styles);
 
 const MainPage: NextPage = ({ data = main, partners }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  console.log(data)
   const { first_screen, afisha, blog, news, banners, places, video_archive, short_list } = data;
 
   const [displayFirstScreen, setDisplayFirstScreen] = useState(false);
