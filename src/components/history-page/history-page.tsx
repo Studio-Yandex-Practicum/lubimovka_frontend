5b583766f986aaae685278190b5241f998cd759c
gremwiz1 <gremwiz@yandex.ru>
2022-01-26 17:27:22 +0300

    refactoring history page

--- src/components/history-page/history-page.tsx
+++ src/components/history-page/history-page.tsx
@@ -1,29 +1,90 @@
-import { FC } from 'react';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { useState } from 'react';
 import Head from 'next/head';
 
 import { HistoryHeader } from './header';
 import { HistoryTitle } from './title';
 import { HistoryItself } from './itself';
+import { fetcher } from 'shared/fetcher';
+import { Festival, Years } from 'api-typings';
 
-import headerData from './assets/mock-data-header.json';
-import titleData from './assets/mock-data-title.json';
 import itselfData from './assets/mock-data-itself.json';
 
-interface IHistoryPageProps {
-  metaTitle: string;
-}
-export const HistoryPage: FC<IHistoryPageProps> = (props: IHistoryPageProps) => {
+export const HistoryPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const {
-    metaTitle,
+    years, titleCounts
   } = props;
+  const [currentTitleData, setCurrentTitleData] = useState(titleCounts);
+  const [currentYear, setCurrentYear] = useState(years ? (years.years[0] ? years.years[0] : 2022) : 2022);
+console.log(years);
+console.log(titleCounts)
+  function selectYear(year: number ) {
+    if(year) {
+      setCurrentYear(year);
+      fetchStatistics(year)
+        .then((result) => {
+          if(result) {
+            setCurrentTitleData(result);
+          }
+        }).catch((error) => {
+          throw(error);
+        });
+    }
+  }
   return (
     <>
       <Head>
-        <title>{metaTitle}</title>
+
       </Head>
-      <HistoryHeader data={headerData}/>
-      <HistoryTitle data={titleData}/>
+
       <HistoryItself data={itselfData}/>
     </>
   );
+};
+/*
+<HistoryHeader data={years} selectYear={selectYear}/>
+      <HistoryTitle data={currentTitleData} currentYear={currentYear}/>
+      */
+const fetchStatistics = async (year: number) => {
+  let data;
+  try {
+    data = await fetcher<Festival>(`/info/festivals/${year}/`);
+  } catch (error) {
+    return;
+  }
+  return data;
+};
+const fetchInitStateYear = async () => {
+  let data;
+  try {
+    data = await fetcher<Years>('/info/festivals/years/');
+  } catch (error) {
+    return;
+  }
+  return data;
+};
+type History = {
+  titleCounts : Festival,
+  years : Years
+}
+export const getServerSideProps: GetServerSideProps<History> = async () => {
+  const years = await fetchInitStateYear();
+  if (!years) {
+    return {
+      notFound: true,
+    };
+  }
+  else {
+    const titleCounts = await fetchStatistics(years.years[0]);
+    if(!titleCounts) {
+      return {
+        notFound: true,
+      };
+    }
+    return {
+      props: {
+        titleCounts: titleCounts, years: years
+      },
+    };
+  }
 };
