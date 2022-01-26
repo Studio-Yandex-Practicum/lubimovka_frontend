7f0f65db850263c3b791c464d4cb20d0c6071ddd
Rustam <Rustamaaaa@bk.ru>
2022-01-26 14:00:59 +0300

    fix/правки

--- src/components/what-we-do-page/header/what-we-do-header.tsx
+++ src/components/what-we-do-page/header/what-we-do-header.tsx
@@ -1,5 +1,5 @@
 
-import { Image } from 'next';
+import Image from 'next/image';
 import cn from 'classnames/bind';
 
 import { AboutUsMenu } from 'components/what-we-do-page/about-us-menu';
@@ -10,10 +10,10 @@ const cx = cn.bind(styles);
 
 export const WhatWeDoHeader = (): JSX.Element => (
   <section className={cx('header')}>
+    <nav className={cx('menu')}>
+      <AboutUsMenu/>
+    </nav>
     <div className={cx('content')}>
-      <div className={cx('menu')}>
-        <AboutUsMenu/>
-      </div>
       <h1 className={cx('title')}>
           Фестиваль молодой драматургии Любимовка —
       </h1>
@@ -27,11 +27,14 @@ export const WhatWeDoHeader = (): JSX.Element => (
       </div>
     </div>
     <div className={cx('container')}>
-      <img
-        src='/images/what-we-do/header.jpg'
-        className={cx('img')}
-        alt='Что мы делаем?'
-      />
+      <div className={cx('wrapper')}>
+        <Image
+          src="/images/what-we-do/header.jpg"
+          alt="Что мы делаем?"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
       <div className={cx('containerSpace')}>
         <div className={cx('containerSpaceUp')}/>
         <div className={cx('containerSpaceDown')}/>
