1504c9f3dda685ceb8b41527daa2549bcd5ff534
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-01-29 17:16:23 +0300

    feat: добавление вывода ошибок валидации полей формы подачи пьесы из ответа api

--- src/pages/form/form.tsx
+++ src/pages/form/form.tsx
@@ -14,6 +14,7 @@ import {
 } from 'shared/constants/regexps';
 import { Nullable } from 'shared/types';
 import { fetcher } from 'shared/fetcher';
+import { snakeToCamel } from 'shared/helpers/snake-to-camel';
 
 interface ParticipationFormFields {
   firstName: string,
@@ -22,28 +23,29 @@ interface ParticipationFormFields {
   city: string,
   phoneNumber: string,
   email: string,
-  playTitle: string,
-  playYear: string,
-  playFile: Nullable<File>,
+  title: string,
+  year: string,
+  file: Nullable<File>,
 }
 
 enum ParticipationFormActionTypes {
   FieldChange,
-  Reset,
+  FieldError,
 }
 
 type ParticipationFormAction<K extends keyof ParticipationFormFields = keyof ParticipationFormFields> =
-  | { type: ParticipationFormActionTypes.FieldChange, payload: { field: K, value: ParticipationFormFields[K] } }
-  | { type: ParticipationFormActionTypes.Reset }
+  { type: ParticipationFormActionTypes.FieldChange, payload: { field: K, value: ParticipationFormFields[K], error?: string } }
+  | { type: ParticipationFormActionTypes.FieldError, payload: { field: K, error: string } }
 
 type ParticipationFormStateFields<T> = {
   [K in keyof T]: {
     value: T[K],
     wasChanged: boolean,
+    error?: string,
   }
 }
 
-type ParticipationFormState = ParticipationFormStateFields<ParticipationFormFields>
+type ParticipationFormState = ParticipationFormStateFields<ParticipationFormFields>;
 
 const initialParticipationFormState: ParticipationFormState = {
   firstName: { value: '', wasChanged: false },
@@ -52,9 +54,9 @@ const initialParticipationFormState: ParticipationFormState = {
   city: { value: '', wasChanged: false },
   phoneNumber: { value: '', wasChanged: false },
   email: { value: '', wasChanged: false },
-  playTitle: { value: '', wasChanged: false },
-  playYear: { value: '', wasChanged: false },
-  playFile: { value: null, wasChanged: false },
+  title: { value: '', wasChanged: false },
+  year: { value: '', wasChanged: false },
+  file: { value: null, wasChanged: false },
 };
 
 const participationFormReducer = (state: ParticipationFormState, action: ParticipationFormAction) => {
@@ -65,17 +67,24 @@ const participationFormReducer = (state: ParticipationFormState, action: Partici
       [action.payload.field]: {
         value: action.payload.value,
         wasChanged: true,
+        error: action.payload.error,
       },
     };
-  case ParticipationFormActionTypes.Reset:
-    return initialParticipationFormState;
+  case ParticipationFormActionTypes.FieldError:
+    return {
+      ...state,
+      [action.payload.field]: {
+        ...state[action.payload.field],
+        error: action.payload.error,
+      },
+    };
   default:
     return state;
   }
 };
 
 const Participation: NextPage = () => {
-  const [formState, dispatch] = useReducer(participationFormReducer, initialParticipationFormState);
+  const [participationFormState, dispatch] = useReducer(participationFormReducer, initialParticipationFormState);
 
   const {
     firstName,
@@ -84,145 +93,138 @@ const Participation: NextPage = () => {
     city,
     phoneNumber,
     email,
-    playTitle,
-    playYear,
-    playFile,
-  } = formState;
+    title,
+    year,
+    file,
+  } = participationFormState;
 
   const router = useRouter();
 
-  const getFirstNameError = () => {
-    if (firstName.value.length < 2) {
-      return 'Имя должно содержать минимум 2 символа';
-    }
+  const getFieldError = <K extends keyof ParticipationFormFields>(field: K, value: ParticipationFormFields[K]) => {
+    switch (field) {
+    case 'firstName':
+      if ((value as ParticipationFormFields['firstName']).length < 2) {
+        return 'Имя должно содержать минимум 2 символа';
+      }
+      break;
+    case 'lastName':
+      if ((value as ParticipationFormFields['lastName']).length < 2) {
+        return 'Фамилия должна содержать минимум 2 символа';
+      }
+      break;
+    case 'birthYear':
+      if (!validYearRegexp.test((value as ParticipationFormFields['birthYear']))) {
+        return 'Неверный год рождения';
+      }
+      break;
+    case 'city':
+      if ((value as ParticipationFormFields['city']).length < 2) {
+        return 'Город должен содержать минимум 2 символа';
+      }
+      break;
+    case 'phoneNumber':
+      if (!validPhoneNumberRegexp.test((value as ParticipationFormFields['phoneNumber']))) {
+        return 'Некорректный номер телефона';
+      }
+      break;
+    case 'email':
+      if (!(value as ParticipationFormFields['email']).length) {
+        return 'Поле E-mail обязательно для заполнения';
+      }
+      if (!validEmailRegexp.test((value as ParticipationFormFields['email']))) {
+        return 'Неверный формат адреса электронной почты';
+      }
+      break;
+    case 'title':
+      if (!(value as ParticipationFormFields['title']).length) {
+        return 'Название обязательно для заполнения';
+      }
+      break;
+    case 'year':
+      if (!validYearRegexp.test((value as ParticipationFormFields['year']))) {
+        return 'Неверный год';
+      }
+      break;
+    case 'file':
+      if (!value) {
+        return 'Файл обязателен';
+      }
 
-    return;
+      const { name } = value as Exclude<ParticipationFormFields['file'], null>;
+
+      if (/[а-яА-ЯЁё]/.test(name)) {
+        return 'Файл содержит кириллицу, пожалуйста, переименуйте его.';
+      }
+      if (/[^A-Za-z._-]/.test(name)) {
+        return 'Пожалуйста, используйте только латинские символы и знаки - и _';
+      }
+      break;
+    default:
+      return;
+    }
   };
 
-  const getLastNameError = () => {
-    if (lastName.value.length < 2) {
-      return 'Фамилия должна содержать минимум 2 символа';
-    }
-
-    return;
-  };
-
-  const getBirthYearError = () => {
-    if (!validYearRegexp.test(birthYear.value)) {
-      return 'Неверный год рождения';
-    }
-
-    return;
-  };
-
-  const getCityError = () => {
-    if (city.value.length < 2) {
-      return 'Город должен содержать минимум 2 символа';
-    }
-
-    return;
-  };
-
-  const getPhoneNumberError = () => {
-    if (!validPhoneNumberRegexp.test(phoneNumber.value)) {
-      return 'Некорректный номер телефона';
-    }
-
-    return;
-  };
-
-  const getEmailError = () => {
-    if (!email.value.length) {
-      return 'Поле E-mail обязательно для заполнения';
-    }
-
-    if (!validEmailRegexp.test(email.value)) {
-      return 'Неверный формат адреса электронной почты';
-    }
-
-    return;
-  };
-
-  const getPlayTitleError = () => {
-    if (!playTitle.value.length) {
-      return 'Название обязательно для заполнения';
-    }
-
-    return;
-  };
-
-  const getPlayYearError = () => {
-    if (!validYearRegexp.test(playYear.value)) {
-      return 'Неверный год';
-    }
-
-    return;
-  };
-
-  const getPlayFileError = () => {
-    if (!playFile.value) {
-      return 'Файл обязателен';
-    }
-
-    if (playFile.value && /[а-яА-ЯЁё]/.test(playFile.value.name)) {
-      return 'Файл содержит кириллицу, пожалуйста, переименуйте его.';
-    }
-
-    if (playFile.value && /[^A-Za-z._-]/.test(playFile.value.name)) {
-      return 'Пожалуйста, используйте только латинские символы и знаки - и _';
-    }
-
-    return;
-  };
-
-  const handleFieldChange = (field: keyof ParticipationFormState) => (value: ParticipationFormFields[typeof field]) => {
+  const handleFieldChange = <K extends keyof ParticipationFormFields>(field: K) => (value: ParticipationFormFields[K]) => {
     dispatch({
       type: ParticipationFormActionTypes.FieldChange,
       payload: {
         field,
         value,
+        error: getFieldError(field, value),
       },
     });
   };
 
-  const canSubmit = (
-    !getFirstNameError()
-    && !getLastNameError()
-    && !getBirthYearError()
-    && !getCityError()
-    && !getPhoneNumberError()
-    && !getEmailError()
-    && !getPlayTitleError()
-    && !getPlayYearError()
-    && !getPlayFileError()
-  );
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const data = new FormData();
 
-    data.append('birth_year', birthYear.value);
     data.append('first_name', firstName.value);
     data.append('last_name', lastName.value);
+    data.append('birth_year', birthYear.value);
     data.append('city', city.value);
     data.append('phone_number', phoneNumber.value);
     data.append('email', email.value);
-    data.append('title', playTitle.value);
-    data.append('year', playYear.value);
-    data.append('file', playFile.value!);
+    data.append('title', title.value);
+    data.append('year', year.value);
+    data.append('file', file.value!);
 
     try {
-      fetcher('/library/participation/', {
+      await fetcher('/library/participation/', {
         method: 'POST',
         body: data,
       });
     } catch (error) {
+      // TODO: добавить проверку типов выброшенного исключения, пока считаем, что всегда получаем ответ API
+
+      for (let field in error as Record<string, string[]>) {
+        dispatch({
+          type: ParticipationFormActionTypes.FieldError,
+          payload: {
+            field: snakeToCamel(field) as keyof ParticipationFormFields,
+            error: (error as Record<string, string[]>)[field][0],
+          },
+        });
+      }
+
       return;
     }
 
     router.push('/form/success');
   };
+
+  const canSubmit = (
+    firstName.wasChanged && !firstName.error
+    && lastName.wasChanged && !lastName.error
+    && birthYear.wasChanged && !birthYear.error
+    && city.wasChanged && !city.error
+    && phoneNumber.wasChanged && !phoneNumber.error
+    && email.wasChanged && !email.error
+    && title.wasChanged && !title.error
+    && year.wasChanged && !year.error
+    && file.wasChanged && !file.error
+  );
 
   return (
     <AppLayout>
@@ -241,31 +243,31 @@ const Participation: NextPage = () => {
             <ParticipationForm
               firstName={firstName.value}
               onFirstNameChange={handleFieldChange('firstName')}
-              firstNameError={firstName.wasChanged ? getFirstNameError() : undefined}
+              firstNameError={firstName.wasChanged ? firstName.error : undefined}
               lastName={lastName.value}
               onLastNameChange={handleFieldChange('lastName')}
-              lastNameError={lastName.wasChanged ? getLastNameError() : undefined}
+              lastNameError={lastName.wasChanged ? lastName.error : undefined}
               birthYear={birthYear.value}
               onBirthYearChange={handleFieldChange('birthYear')}
-              birthYearError={birthYear.wasChanged ? getBirthYearError() : undefined}
+              birthYearError={birthYear.wasChanged ? birthYear.error : undefined}
               city={city.value}
-              cityError={city.wasChanged ? getCityError() : undefined}
+              cityError={city.wasChanged ? city.error : undefined}
               onCityChange={handleFieldChange('city')}
               phoneNumber={phoneNumber.value}
               onPhoneNumberChange={handleFieldChange('phoneNumber')}
-              phoneNumberError={phoneNumber.wasChanged ? getPhoneNumberError() : undefined}
+              phoneNumberError={phoneNumber.wasChanged ? phoneNumber.error : undefined}
               email={email.value}
               onEmailChange={handleFieldChange('email')}
-              emailError={email.wasChanged ? getEmailError() : undefined}
-              playTitle={playTitle.value}
-              playTitleError={playTitle.wasChanged ? getPlayTitleError() : undefined}
-              onPlayTitleChange={handleFieldChange('playTitle')}
-              playYear={playYear.value}
-              playYearError={playYear.wasChanged ? getPlayYearError() : undefined}
-              onPlayYearChange={handleFieldChange('playYear')}
-              playFileName={playFile.value ? playFile.value.name : undefined}
-              playFileError={playFile.wasChanged ? getPlayFileError() : undefined}
-              onPlayFileChange={handleFieldChange('playFile')}
+              emailError={email.wasChanged ? email.error : undefined}
+              title={title.value}
+              titleError={title.wasChanged ? title.error : undefined}
+              onTitleChange={handleFieldChange('title')}
+              year={year.value}
+              yearError={year.wasChanged ? year.error : undefined}
+              onYearChange={handleFieldChange('year')}
+              fileName={file.value ? file.value.name : undefined}
+              fileError={file.wasChanged ? file.error : undefined}
+              onFileChange={handleFieldChange('file')}
               canSubmit={canSubmit}
               onSubmit={handleSubmit}
             />
