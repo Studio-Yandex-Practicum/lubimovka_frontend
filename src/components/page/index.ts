0457a078782187c48f9fcd88749ffee189ecea46
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-02-02 00:38:47 +0300

    refactor: рефакторинг компонентов общей раскладки и шапки

--- src/components/page/index.ts
+++ src/components/page/index.ts
@@ -1,5 +1,5 @@
 export { default } from './page';
-export { PageHeader } from './header';
+export { PageNavbar } from './navbar';
 export { PageOverlayMenu } from './overlay-menu';
 export { PageBurgerButton } from './burger-button';
 export { PageFooter } from './footer';
