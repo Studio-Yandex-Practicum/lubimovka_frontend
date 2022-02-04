334cd49f08a2f356300d0b28dd131aa3481fade1
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-02-04 23:49:10 +0300

    fix: исправление ошибок после ребейза

--- src/components/afisha-page/regular-events/afisha-regular-events.tsx
+++ src/components/afisha-page/regular-events/afisha-regular-events.tsx
@@ -2,29 +2,31 @@ import { FC } from 'react';
 import cn from 'classnames/bind';
 
 import { AnnouncedPlayCard, IAnnouncedPlayCardProps } from 'components/ui/announced-play-card';
+import { formatDate, formatTime } from 'shared/helpers/formatDateServerData';
 
 import styles from './afisha-regular-events.module.css';
 
 const cx = cn.bind(styles);
 
 interface IRegularEventsProps {
-    data: IAnnouncedPlayCardProps[];
+  data: (Omit<IAnnouncedPlayCardProps, 'formattedDate' | 'formattedTime'> & { date_time: string })[]
 }
 
 export const RegularEvents: FC<IRegularEventsProps> = (props) => {
   const { data } = props;
+
   return (
     <section className={cx('section')}>
       {data.map((event) => (
         <AnnouncedPlayCard
           key={event.id}
-          date={event.date}
+          formattedDate={formatDate(event.date_time)}
+          formattedTime={formatTime(event.date_time)}
           title={event.title}
-          dramatists={event.dramatists}
-          directors={event.directors}
+          team={event.team}
           buttonLink={event.buttonLink}
           className={styles.event}
-          projectText={event.projectText}
+          project={event.project}
           isPerformance={event.isPerformance}
           imageUrl={event.imageUrl}
           description={event.description}
