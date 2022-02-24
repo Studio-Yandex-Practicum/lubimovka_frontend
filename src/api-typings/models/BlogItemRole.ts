4db5e6888f54095b887565119a2df68031c15cf8
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-02-24 09:29:57 +0300

    chore: обновление типов API

---
+++ src/api-typings/models/BlogItemRole.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type BlogItemRole = {
+    name: string;
+    /**
+     * Если пустое, то заполняется автоматически
+     */
+    slug: string;
+    readonly persons: Array<string>;
+};
