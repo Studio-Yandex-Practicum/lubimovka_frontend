741616b179a3b7f5482cb55e5d5386390019baaf
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-02-28 10:21:00 +0300

    refactor: рефакторинг страницы блога

--- src/components/blog-entry-list/blog-entry-list.tsx
+++ src/components/blog-entry-list/blog-entry-list.tsx
@@ -1,13 +1,17 @@
-import { ReactNode, useRef, useEffect } from 'react';
+import { ReactNode, useRef, useEffect, useState, Children } from 'react';
 import classNames from 'classnames/bind';
 
 import { BlogEntryListItem } from './item';
+import { useDidMountEffect } from 'shared/hooks/use-did-mount-effect';
+import { useIntersection } from 'shared/hooks/use-intersection';
 
 import styles from './blog-entry-list.module.css';
 import blogEntryListVars from './blog-entry-list.vars.module.css';
 
 interface IBlogEntryListProps {
   children: ReactNode
+  hasMoreEntries: boolean
+  onShouldLoadEntries: () => void
 }
 
 const cx = classNames.bind(styles);
@@ -16,9 +20,16 @@ const columnCount = parseInt(blogEntryListVars['column-count-tablet-portrait'], 
 const firstItemColumnSpan = parseInt(blogEntryListVars['first-item-column-span-tablet-portrait'], 10);
 const previousColumnIndexOffset = firstItemColumnSpan - 1;
 
-export const BlogEntryList = (props: IBlogEntryListProps): JSX.Element => {
-  const { children } = props;
+export const BlogEntryList = (props: IBlogEntryListProps) => {
+  const {
+    children,
+    hasMoreEntries,
+    onShouldLoadEntries,
+  } = props;
   const containerRef = useRef<HTMLUListElement>(null);
+  const lastEntryRef = useRef<HTMLSpanElement>(null);
+  const shouldLoadEntries = useIntersection(lastEntryRef, { threshold: .85 });
+  const [loading, setLoading] = useState(false);
 
   const layout = () => {
     // https://css-tricks.com/a-lightweight-masonry-solution/
@@ -32,13 +43,26 @@ export const BlogEntryList = (props: IBlogEntryListProps): JSX.Element => {
 
       element.style.marginTop = `${previousElementBottom + 60 - currentElementTop}px`;
     });
+
+    setLoading(false);
   };
 
   useEffect(() => {
     if (!containerRef.current || getComputedStyle(containerRef.current).gridTemplateRows === 'masonry') return;
 
     window.addEventListener('load', layout);
-  }, []);
+  });
+
+  useDidMountEffect(() => {
+    layout();
+  }, [Children.count(children)]);
+
+  useEffect(() => {
+    if (!loading && shouldLoadEntries && hasMoreEntries) {
+      setLoading(true);
+      onShouldLoadEntries();
+    }
+  }, [hasMoreEntries, shouldLoadEntries, loading, onShouldLoadEntries]);
 
   return (
     <ul
@@ -46,6 +70,7 @@ export const BlogEntryList = (props: IBlogEntryListProps): JSX.Element => {
       ref={containerRef}
     >
       {children}
+      <span ref={lastEntryRef}/>
     </ul>
   );
 };
