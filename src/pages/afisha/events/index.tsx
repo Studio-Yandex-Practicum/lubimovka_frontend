a9ab881d8f0d2fc769fab042e838cbe36f68c89a
Pyotr <PePeGrigorchenko@yandex.ru>
2022-03-01 22:04:11 +0300

    test

--- src/pages/afisha/events/index.tsx
+++ src/pages/afisha/events/index.tsx
@@ -1,10 +1,11 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 import cn from 'classnames/bind';
 
 import { AfishaTitle } from 'components/afisha-page/title';
 import { FestivalDays } from 'components/afisha-page/festival-days';
 import { RegularEvents } from 'components/afisha-page/regular-events';
 import { AppLayout } from 'components/app-layout';
+import { fetcher } from 'shared/fetcher';
 
 import data from '../../../mocks/data/afisha.json';
 import festivalData from '../../../mocks/assets/afisha/afisha-fesival-data.json';
@@ -47,6 +48,31 @@ const Afisha: NextPage<IAfisheProps> = () => {
       </main>
     </AppLayout>
   );
+};
+
+const fetchEvents = async (offset?: number) => {
+  return await fetcher<any>(`afisha/_events/?limit=10${offset ? `&offset=${offset}` : ''}`);
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  let response;
+
+  try {
+    response = await fetchEvents();
+  } catch (err) {
+    return {
+      props: {
+        errorCode: 500,
+      }
+    };
+  }
+
+  return {
+    props: {
+      blogEntries: response.results,
+      hasMoreEntries: !!response.next,
+    }
+  };
 };
 
 export default Afisha;
