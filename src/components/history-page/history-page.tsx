151bd26318daa7a73421561e640e65948b709fa7
gremwiz1 <gremwiz@yandex.ru>
2022-03-07 16:33:25 +0300

    refactoring history-title

--- src/components/history-page/history-page.tsx
+++ src/components/history-page/history-page.tsx
@@ -1,19 +1,20 @@
 import { useState, FC } from 'react';
 import Head from 'next/head';
+import { Festival, Years, PlayFilters } from 'api-typings';
 
 import { HistoryHeader } from './header';
 import { HistoryTitle } from './title';
 import { HistoryItself } from './itself';
 import { fetcher } from 'shared/fetcher';
-import { Festival, Years } from 'api-typings';
 
 import itselfData from './assets/mock-data-itself.json';
 
 interface IHistoryPage  {
   years: Years,
-  titleCounts: Festival
+  titleCounts: Festival,
+  playFilters: PlayFilters
 }
-export const HistoryPage: FC<IHistoryPage> = ({ years, titleCounts }) => {
+export const HistoryPage: FC<IHistoryPage> = ({ years, titleCounts, playFilters }) => {
   const [currentTitleData, setCurrentTitleData] = useState(titleCounts);
   const [currentYear, setCurrentYear] = useState(years.years[0]);
 
@@ -35,7 +36,7 @@ export const HistoryPage: FC<IHistoryPage> = ({ years, titleCounts }) => {
       <Head>
       </Head>
       <HistoryHeader data={years} selectYear={selectYear}/>
-      <HistoryTitle data={currentTitleData} currentYear={currentYear}/>
+      <HistoryTitle data={currentTitleData} currentYear={currentYear} playFilters={playFilters}/>
       <HistoryItself data={itselfData}/>
 
     </>
