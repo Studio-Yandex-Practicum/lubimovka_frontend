3163e0fe09d357d755335467027976d660abc084
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-03-12 16:49:42 +0300

    refactor: рефакторинг хелпера форматирования даты и времени

--- src/components/main-page/events/main-events.tsx
+++ src/components/main-page/events/main-events.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames/bind';
 
 import { AnnouncedPlayCard } from 'components/ui/announced-play-card';
 import { IMainAfisha } from './main-events.props';
-import { formatDate, formatTime } from 'shared/helpers/formatDateServerData';
+import { formatDateTime } from 'shared/helpers/format-date-time';
 import { main } from 'mocks/data/main';
 
 import styles from './main-events.module.css';
@@ -20,8 +20,8 @@ export const MainEvents: FC<IMainAfisha> = () => {
             <li key={item.id} className={cx('list')}>
               <AnnouncedPlayCard
                 id={item.id}
-                formattedDate={formatDate(item.date_time)}
-                formattedTime={formatTime(item.date_time)}
+                formattedDate={formatDateTime(item.date_time, 'dMMMM')}
+                formattedTime={formatDateTime(item.date_time, 'mH')}
                 title={item.event_body.name}
                 team={item.event_body.team}
                 description={item.event_body.description}
