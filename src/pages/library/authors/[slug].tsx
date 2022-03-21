2fd8368e15c557b54ed7f1af2f61ae2ca454bcb0
Pyotr Grigorchenko <PePeGrigorchenko@yandex.ru>
2022-03-21 20:50:47 +0300

    Author id=>slug

--- src/pages/library/authors/[slug].tsx
+++ src/pages/library/authors/[slug].tsx
@@ -29,10 +29,10 @@ const fetchAuthors = async (authorId: string) => {
   return data;
 };
 
-export const getServerSideProps: GetServerSideProps<AuthorRetrieveModel, Record<'id', string>> = async ({ params }) => {
-  const { id: authorId } = params!;
+export const getServerSideProps: GetServerSideProps<AuthorRetrieveModel, Record<'slug', string>> = async ({ params }) => {
+  const { slug: authorSlug } = params!;
 
-  const author = await fetchAuthors(authorId);
+  const author = await fetchAuthors(authorSlug);
 
   if (!author) {
     return {
