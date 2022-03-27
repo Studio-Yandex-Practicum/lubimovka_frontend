19ae3bda4d57b1ef9f21705d23343bf68b5ed0d5
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-03-28 00:03:13 +0300

    chore: приведение фавиконки к дизайну

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-import { Favicon } from '../components/favicon'
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <Favicon />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      {/* https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs */}
+      <link rel="icon" href="/favicon.ico" sizes="any"/>
+      <link rel="icon" href="/icon.svg" type="image/svg+xml"/>
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+      <link rel="manifest" href="/manifest.webmanifest"/>
+    </Head>
+    <body>
+      <Main/>
+      <NextScript/>
+    </body>
+  </Html>
+);
+
+export default Document;
