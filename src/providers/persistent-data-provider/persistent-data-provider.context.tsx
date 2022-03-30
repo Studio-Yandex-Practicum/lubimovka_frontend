c623c30b2688ad0fd4b8171bdf98e24547e33898
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-03-30 22:38:27 +0300

    feat: получение настроек сайта с бекенда

--- src/providers/persistent-data-provider/persistent-data-provider.context.tsx
+++ src/providers/persistent-data-provider/persistent-data-provider.context.tsx
@@ -1,15 +1,34 @@
 import { createContext, useContext } from 'react';
 
-import { Project, Partner } from 'shared/types';
+import type { Project, Partner, Url } from 'shared/types';
 
-export type PersistentDataContext = {
-  projects?: Project[],
-  partners?: Partner[],
+export type Settings = {
+  emailAddresses: {
+    forDirectorsAndActors: string
+    forDirectors: string
+    forAuthors: string
+    forVolunteers: string
+    charity: string
+    playAcceptance: string
+  },
+  pressCenter: {
+    contactPerson: string
+    contactPersonPhoto: Url
+    contactEmail: string
+    facebookGalleryUrl: string
+  },
+  canProposePlay: boolean
 }
 
-export const PersistentDataContext = createContext<PersistentDataContext | undefined>(undefined);
+export type PersistentDataContextType = {
+  projects?: Project[]
+  partners?: Partner[]
+  settings?: Settings
+}
 
-export const usePersistentData = (): PersistentDataContext => {
+export const PersistentDataContext = createContext<PersistentDataContextType | undefined>(undefined);
+
+export const usePersistentData = (): PersistentDataContextType => {
   const context = useContext(PersistentDataContext);
 
   if (context === undefined) {
