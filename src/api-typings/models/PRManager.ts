6bc1cf6859f117aad61cf8292d71fa55fd3e5e30
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-04-07 16:20:58 +0300

    chore: обновление типов API

--- src/api-typings/models/PRManager.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type PRManager = {
-    pr_manager_name: string;
-    pr_manager_email: string;
-    pr_manager_photo_link: string;
-};
