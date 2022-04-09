c96abc50619ac33f0812e6da6a361cf137571da7
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-04-09 11:38:09 +0300

    refactor: перемещение кастомного документа в каталог компонентов

---
+++ src/components/document/document.tsx
@@ -0,0 +1,25 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
+import {
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
+
+const Document = () => (
+  <Html>
+    <Head>
+      {/* https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs */}
+      <link rel="icon" href="/favicon.ico" sizes="any"/>
+      <link rel="icon" href="/icon.svg" type="image/svg+xml"/>
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+      <link rel="manifest" href="/manifest.webmanifest"/>
+    </Head>
+    <body>
+      <Main/>
+      <NextScript/>
+    </body>
+  </Html>
+);
+
+export default Document;
