689d16f6d604b73ca97b84f5d7460fa85f16e9b0
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-04-10 14:33:24 +0300

    fix: исправление рендеринга меты страниц на стороне сервера

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -21,6 +21,11 @@ export const SEO: FC<SEOProps> = (props) => {
 
   return (
     <Head>
+      {/* https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs */}
+      <link rel="icon" href="/favicon.ico" sizes="any"/>
+      <link rel="icon" href="/icon.svg" type="image/svg+xml"/>
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+      <link rel="manifest" href="/manifest.webmanifest"/>
       <title>
         {`${title} - ${settings.defaultMeta.title}`}
       </title>
