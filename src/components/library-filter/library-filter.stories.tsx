1d7f1ab3e28f3e773be41ec1d18ef111a9b068ec
Evgeniy Karmyskov <karmyskove@gmail.com>
2022-10-15 18:55:02 +0700

    fix: поднял LibraryFilter StoryBook

--- src/components/library-filter/library-filter.stories.tsx
+++ src/components/library-filter/library-filter.stories.tsx
@@ -2,12 +2,16 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import LibraryFilter from './library-filter';
 
+const fakeYears = Array.from({ length: 8 }, () => ({ value: 2022, text: '2022' }));
+
+const fakeProgrammes = Array.from({ length: 4 }, () => ({ pk: 2, name: 'Шорт-лист' }));
+
 export default {
   title: 'Components/LibraryFilter',
   component: LibraryFilter
 } as ComponentMeta<typeof LibraryFilter>;
 
-const Template: ComponentStory<typeof LibraryFilter> = (args) => <LibraryFilter {...args}/>;
+const Template: ComponentStory<typeof LibraryFilter> = (args) => <LibraryFilter {...args} years={fakeYears} programmes={fakeProgrammes}/>;
 
 export const Default = Template.bind({});
 
