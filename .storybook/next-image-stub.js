8f3fc38019f3c6f6db32532cc3e5840e99c19628
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-10-29 18:52:25 +0300

    chore: добавление стаба NextImage

---
+++ .storybook/next-image-stub.js
@@ -0,0 +1,12 @@
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
+});
