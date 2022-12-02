6500c877a9bb50b3c5366eadc7971d8f7be0a988
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-12-02 19:01:28 +0300

    chore: фиксация хедера

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import classNames from 'classnames/bind';
 
 import { MainLayout } from 'components/main-layout';
 import { FeedList } from 'components/feed-list';
@@ -12,12 +11,11 @@ import { Section } from 'components/section';
 import { PartnerList } from 'components/partner-list';
 import { AddressList } from 'components/address-list';
 import { AddressCard } from 'components/address-card';
+import { MainHeader } from 'components/main-header';
 import { HomepageFeedSection } from 'components/homepage-feed-section';
 import { HomepageVideoArchiveSection } from 'components/homepage-video-archive-section';
-import { HomepageHeadline } from 'components/homepage-headline';
 import { HomepageEventsSection } from 'components/homepage-events-section';
 import { AppLayout } from 'components/app-layout';
-import { Banner } from 'components/banner';
 import { PlayCard } from 'components/play-card';
 import { PlayList } from 'components/play-list';
 import { EventList } from 'components/event-list';
@@ -27,16 +25,11 @@ import { SEO } from 'components/seo';
 import { fetcher } from 'services/fetcher';
 import { getPartners } from 'services/api/partners';
 import { format } from 'shared/helpers/format-date';
-import { Partner, PartnerType } from 'core/partner';
+import { PartnerType } from 'core/partner';
 
 import type { InferGetServerSidePropsType } from 'next';
-import type {
-  Main as MainPageData,
-} from '__generated__/api-typings';
-
-import styles from 'components/main-layout/main-layout.module.css';
-
-const cx = classNames.bind(styles);
+import type { Partner } from 'core/partner';
+import type { Main as MainPageData } from '__generated__/api-typings';
 
 const Main = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const {
@@ -54,209 +47,203 @@ const Main = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => 
   const getPartnersGroups = () => Object.keys(partners) as (keyof typeof PartnerType)[];
 
   return (
-    <AppLayout
-      hiddenPartners
-      navbarProps={{
-        view: first_screen ? 'expanded': 'normal',
-      }}
-      headBanner={first_screen ? (
-        <Banner image={first_screen.image}/>
-      ) : undefined}
-    >
+    <>
       <SEO title="Главная"/>
-      <MainLayout>
+      <AppLayout noNavbar={!!first_screen}>
         {first_screen && (
-          <HomepageHeadline
-            className={cx('headline')}
+          <MainHeader
+            cover={first_screen.image}
             title={first_screen.title}
-            url={first_screen.url}
-            callToAction={first_screen.url_title}
+            actionText={first_screen.url_title}
+            actionUrl={first_screen.url}
           />
         )}
-        {!!afisha?.items.length && (
-          <MainLayout.Events>
-            <HomepageEventsSection
-              title={afisha.afisha_today ? (
-                <>
-                  Афиша на сегодня,
-                  {' '}
-                  {format('d MMMM', new Date())}
-                </>
-              ) : (
-                <>
-                  Афиша
-                  <br/>
-                  событий
-                </>
-              )}
-              description={afisha!.description}
-            >
-              <EventList>
-                {afisha.items.map((event) => (
-                  <EventList.Item key={event.id}>
-                    <EventCard
-                      // TODO: разобраться, сча в схеме API нет поля с изображением
-                      // @ts-expect-error
-                      imageUrl={event.event_body.image}
-                      date={format('d MMMM', new Date(event.date_time))}
-                      time={format('H:mm', new Date(event.date_time))}
-                      title={event.event_body.name}
-                      team={event.event_body.team}
-                      projectTitle={event.event_body.project_title}
-                      description={event.event_body.description}
-                      {...event.type === 'PERFORMANCE' ? {
-                        performanceUrl: `/performances/${event.event_body.id}`,
-                      } : {}}
-                      actionUrl={event.action_url}
-                      actionText={event.action_text}
-                    />
-                  </EventList.Item>
-                ))}
-              </EventList>
-            </HomepageEventsSection>
-          </MainLayout.Events>
-        )}
-        {(blog || news) && (
-          <MainLayout.Feed>
-            <HomepageFeedSection
-              title={blog ? 'Дневник фестиваля' : 'Новости'}
-              action={(
-                <Link
-                  href={blog ? '/blog' : '/news'}
-                  passHref
-                >
-                  <Button
-                    size="s"
-                    border="bottom-left"
-                    upperCase
-                    icon={(
-                      <Icon
-                        glyph="arrow-right"
-                        width="100%"
-                        height="100%"
-                      />
-                    )}
-                  >
-                    Все записи
-                  </Button>
-                </Link>
-              )}
-            >
-              <FeedList>
-                {blog ? (
-                  blog.items.map((entry) => (
-                    <FeedList.Item key={entry.id}>
-                      <BlogCard
-                        id={entry.id}
-                        image={entry.image}
-                        author={entry.author_url_title}
-                        heading={entry.title}
-                        description={entry.description}
-                      />
-                    </FeedList.Item>
-                  ))
+        <MainLayout>
+          {!!afisha?.items.length && (
+            <MainLayout.Events>
+              <HomepageEventsSection
+                title={afisha.afisha_today ? (
+                  <>
+                    Афиша на сегодня,
+                    {' '}
+                    {format('d MMMM', new Date())}
+                  </>
                 ) : (
-                  // @ts-ignore
-                  news.items.map((entry) => (
-                    <FeedList.Item key={entry.id}>
-                      <NewsCard
-                        view="compact"
-                        title={entry.title}
-                        description={entry.description}
-                        date={entry.pub_date && format('d MMMM yyyy', new Date(entry.pub_date))}
-                        href={`/news/${entry.id}`}
-                      />
-                    </FeedList.Item>
-                  ))
+                  <>
+                    Афиша
+                    <br/>
+                    событий
+                  </>
                 )}
-              </FeedList>
-            </HomepageFeedSection>
-          </MainLayout.Feed>
-        )}
-        <MainLayout.Content>
-          {!!banners?.items.length && (
-            <Section type="homepage-teasers">
-              <TeaserList
-                items={banners.items}
+                description={afisha!.description}
+              >
+                <EventList>
+                  {afisha.items.map((event) => (
+                    <EventList.Item key={event.id}>
+                      <EventCard
+                        // TODO: разобраться, сча в схеме API нет поля с изображением
+                        // @ts-expect-error
+                        imageUrl={event.event_body.image}
+                        date={format('d MMMM', new Date(event.date_time))}
+                        time={format('H:mm', new Date(event.date_time))}
+                        title={event.event_body.name}
+                        team={event.event_body.team}
+                        projectTitle={event.event_body.project_title}
+                        description={event.event_body.description}
+                        {...event.type === 'PERFORMANCE' ? {
+                          performanceUrl: `/performances/${event.event_body.id}`,
+                        } : {}}
+                        actionUrl={event.action_url}
+                        actionText={event.action_text}
+                      />
+                    </EventList.Item>
+                  ))}
+                </EventList>
+              </HomepageEventsSection>
+            </MainLayout.Events>
+          )}
+          {(blog || news) && (
+            <MainLayout.Feed>
+              <HomepageFeedSection
+                title={blog ? 'Дневник фестиваля' : 'Новости'}
+                action={(
+                  <Link
+                    href={blog ? '/blog' : '/news'}
+                    passHref
+                  >
+                    <Button
+                      size="s"
+                      border="bottom-left"
+                      upperCase
+                      icon={(
+                        <Icon
+                          glyph="arrow-right"
+                          width="100%"
+                          height="100%"
+                        />
+                      )}
+                    >
+                      Все записи
+                    </Button>
+                  </Link>
+                )}
+              >
+                <FeedList>
+                  {blog ? (
+                    blog.items.map((entry) => (
+                      <FeedList.Item key={entry.id}>
+                        <BlogCard
+                          id={entry.id}
+                          image={entry.image}
+                          author={entry.author_url_title}
+                          heading={entry.title}
+                          description={entry.description}
+                        />
+                      </FeedList.Item>
+                    ))
+                  ) : (
+                    // @ts-ignore
+                    news.items.map((entry) => (
+                      <FeedList.Item key={entry.id}>
+                        <NewsCard
+                          view="compact"
+                          title={entry.title}
+                          description={entry.description}
+                          date={entry.pub_date && format('d MMMM yyyy', new Date(entry.pub_date))}
+                          href={`/news/${entry.id}`}
+                        />
+                      </FeedList.Item>
+                    ))
+                  )}
+                </FeedList>
+              </HomepageFeedSection>
+            </MainLayout.Feed>
+          )}
+          <MainLayout.Content>
+            {!!banners?.items.length && (
+              <Section type="homepage-teasers">
+                <TeaserList
+                  items={banners.items}
+                />
+              </Section>
+            )}
+            {!!short_list?.items.length && (
+              <Section
+                title={short_list.title}
+                type="homepage-shortlist"
+              >
+                <PlayList>
+                  {short_list.items.map((item) => (
+                    <PlayList.Item key={item.id}>
+                      <PlayCard
+                        play={{
+                          title: item.name,
+                          city: item.city,
+                          year: item.year,
+                          readingUrl: item.url_reading,
+                          downloadUrl: item.url_download,
+                          authors: item.authors,
+                        }}
+                      />
+                    </PlayList.Item>
+                  ))}
+                </PlayList>
+              </Section>
+            )}
+            {!!places?.items.length && (
+              <Section
+                type="places"
+                title="Площадки"
+              >
+                <AddressList>
+                  {places.items.map((place) => (
+                    <AddressList.Item key={place.id}>
+                      <AddressCard
+                        title={place.name}
+                        description={place.description}
+                        address={place.address}
+                        url={place.map_link}
+                      />
+                    </AddressList.Item>
+                  ))}
+                </AddressList>
+              </Section>
+            )}
+            {video_archive && (
+              <HomepageVideoArchiveSection
+                url={video_archive.url}
+                image={video_archive.photo}
               />
-            </Section>
-          )}
-          {!!short_list?.items.length && (
-            <Section
-              title={short_list.title}
-              type="homepage-shortlist"
-            >
-              <PlayList>
-                {short_list.items.map((item) => (
-                  <PlayList.Item key={item.id}>
-                    <PlayCard
-                      play={{
-                        title: item.name,
-                        city: item.city,
-                        year: item.year,
-                        readingUrl: item.url_reading,
-                        downloadUrl: item.url_download,
-                        authors: item.authors,
-                      }}
-                    />
-                  </PlayList.Item>
-                ))}
-              </PlayList>
-            </Section>
-          )}
-          {!!places?.items.length && (
-            <Section
-              type="places"
-              title="Площадки"
-            >
-              <AddressList>
-                {places.items.map((place) => (
-                  <AddressList.Item key={place.id}>
-                    <AddressCard
-                      title={place.name}
-                      description={place.description}
-                      address={place.address}
-                      url={place.map_link}
-                    />
-                  </AddressList.Item>
-                ))}
-              </AddressList>
-            </Section>
-          )}
-          {video_archive && (
-            <HomepageVideoArchiveSection
-              url={video_archive.url}
-              image={video_archive.photo}
-            />
-          )}
-          {getPartnersGroups().map((group) => (
-            <Section
-              key={group}
-              type="partners"
-              title={getPartnerGroupTitle(group)}
-            >
-              <PartnerList>
-                {partners[group].map((partner) => (
-                  <PartnerList.Item key={partner.name}>
-                    <PartnerCard
-                      variant="regular"
-                      logo={partner.logo}
-                      name={partner.name}
-                      description={partner.description}
-                      url={partner.url}
-                    />
-                  </PartnerList.Item>
-                ))}
-              </PartnerList>
-            </Section>
-          ))}
-        </MainLayout.Content>
-      </MainLayout>
-    </AppLayout>
+            )}
+            {getPartnersGroups().map((group) => (
+              <Section
+                key={group}
+                type="partners"
+                title={getPartnerGroupTitle(group)}
+              >
+                <PartnerList>
+                  {partners[group].map((partner) => (
+                    <PartnerList.Item key={partner.name}>
+                      <PartnerCard
+                        variant="regular"
+                        logo={partner.logo}
+                        name={partner.name}
+                        description={partner.description}
+                        url={partner.url}
+                      />
+                    </PartnerList.Item>
+                  ))}
+                </PartnerList>
+              </Section>
+            ))}
+          </MainLayout.Content>
+        </MainLayout>
+      </AppLayout>
+    </>
   );
 };
 
-export const getServerSideProps  = async () => {
+export const getServerSideProps = async () => {
   let partners: Partner[] = [];
   const pageData = await fetcher<MainPageData>('/main/');
 
