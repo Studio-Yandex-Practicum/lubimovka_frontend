3a98fd8df7278ed70b5674df05d7fa9a475c4b4f
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2022-12-12 22:47:51 +0300

    chore: добавление тега Google

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,11 @@
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 
 import { PersistentDataProvider } from 'providers/persistent-data-provider';
 import { NewsProvider } from 'providers/news-provider';
 import { BlogProvider } from 'providers/blog-provider';
+
+import { googleAnalyticsTrackingId } from '../../../config/vars';
 
 export const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   const {
@@ -11,12 +14,31 @@ export const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   } = pageProps;
 
   return (
-    <PersistentDataProvider>
-      <NewsProvider preloadedNewsState={preloadedNewsState}>
-        <BlogProvider>
-          <Component {...restPageProps}/>
-        </BlogProvider>
-      </NewsProvider>
-    </PersistentDataProvider>
+    <>
+      {googleAnalyticsTrackingId && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsTrackingId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${googleAnalyticsTrackingId}');
+            `}
+          </Script>
+        </>
+      )}
+      <PersistentDataProvider>
+        <NewsProvider preloadedNewsState={preloadedNewsState}>
+          <BlogProvider>
+            <Component {...restPageProps}/>
+          </BlogProvider>
+        </NewsProvider>
+      </PersistentDataProvider>
+    </>
   );
 };
