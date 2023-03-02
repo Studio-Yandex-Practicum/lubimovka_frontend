96d535ada7eedc13c30967ff4e94deae77ab78c2
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2023-03-02 10:53:20 +0300

    fix: исправление отображения записей блога на главной

--- src/components/feed-list/feed-list.tsx
+++ src/components/feed-list/feed-list.tsx
@@ -1,31 +1,24 @@
 import classNames from 'classnames/bind';
 
-import { FeedListItem } from './item';
-
-import type { ReactNode } from 'react';
-
 import styles from './feed-list.module.css';
 
 interface FeedListProps {
-  children: ReactNode | ReactNode[]
+  variant: 'blog' | 'news'
   className?: string
 }
 
 const cx = classNames.bind(styles);
 
-const Component = (props: FeedListProps) => {
+export const FeedList: React.FC<FeedListProps> = (props) => {
   const {
-    children,
+    variant,
     className,
+    children,
   } = props;
 
   return (
-    <ul className={cx('root', className)}>
+    <ul className={cx(variant, className)}>
       {children}
     </ul>
   );
 };
-
-export const FeedList = Object.assign(Component, {
-  Item: FeedListItem,
-});
