7234f250e1ed85643e21211786d0e9468a77f767
Daria Lazarchuk <dashal_O_o@mail.ru>
2023-08-10 12:35:38 +0300

    fix: hid routes and nav links to team and sponsors

--- src/pages/about-us/sponsors/index.tsx
+++ src/pages/about-us/sponsors/index.tsx
@@ -1,44 +1,60 @@
 
-import { AboutUsLayout } from 'components/about-us-layout';
-import { AppLayout } from 'components/app-layout';
-import PersonsList from 'components/persons-list/persons-list';
-import { SEO } from 'components/seo';
-import SponsorsSection from 'components/sponsors-section/sponsors-section';
-import { useSettings } from 'services/api/settings-adapter';
-import { fetcher } from 'services/fetcher';
+// import { AboutUsLayout } from 'components/about-us-layout';
+// import { AppLayout } from 'components/app-layout';
+// import PersonsList from 'components/persons-list/persons-list';
+// import { SEO } from 'components/seo';
+// import SponsorsSection from 'components/sponsors-section/sponsors-section';
+// import { useSettings } from 'services/api/settings-adapter';
+// import { fetcher } from 'services/fetcher';
 
-import type { Sponsor } from '__generated__/api-typings';
-import type { InferGetServerSidePropsType } from 'next';
+// import type { Sponsor } from '__generated__/api-typings';
+// import type { InferGetServerSidePropsType } from 'next';
 
-const Sponsors = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const { sponsors } = props;
-  const { settings } = useSettings();
+// const Sponsors = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+//   const { sponsors } = props;
+//   const { settings } = useSettings();
 
-  return (
-    <AppLayout>
-      <SEO title="Попечители"/>
-      <AboutUsLayout>
-        <SponsorsSection
-          title="Попечители фестиваля"
-          description="Здесь представлены частные лица и организации, которые помогают Любимовке на постоянной и безвозмездной основе."
-          callToEmail="Если вы хотите стать попечителем фестиваля, напишите нам на "
-          callToEmailAddress={settings?.emailAddresses.sponsorship}
-        >
-          <PersonsList persons={sponsors}/>
-        </SponsorsSection>
-      </AboutUsLayout>
-    </AppLayout>
-  );
-};
+//   return (
+//     <AppLayout>
+//       <SEO title="Попечители"/>
+//       <AboutUsLayout>
+//         <SponsorsSection
+//           title="Попечители фестиваля"
+//           description="Здесь представлены частные лица и организации, которые помогают Любимовке на постоянной и безвозмездной основе."
+//           callToEmail="Если вы хотите стать попечителем фестиваля, напишите нам на "
+//           callToEmailAddress={settings?.emailAddresses.sponsorship}
+//         >
+//           <PersonsList persons={sponsors}/>
+//         </SponsorsSection>
+//       </AboutUsLayout>
+//     </AppLayout>
+//   );
+// };
 
-export const getServerSideProps = async () => {
-  const sponsors = await fetcher<Sponsor[]>('/info/about-festival/sponsors/');
+// export const getServerSideProps = async () => {
+//   const sponsors = await fetcher<Sponsor[]>('/info/about-festival/sponsors/');
 
-  return {
-    props: {
-      sponsors,
-    }
-  };
+//   return {
+//     props: {
+//       sponsors,
+//     }
+//   };
+// };
+
+// export default Sponsors;
+
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const Sponsors = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/about-us/404');
+  }, []);
+
+  return null;
 };
 
 export default Sponsors;
+
--- src/pages/about-us/team/index.tsx
+++ src/pages/about-us/team/index.tsx
@@ -1,45 +1,61 @@
-import { AboutUsLayout } from 'components/about-us-layout';
-import { AppLayout } from 'components/app-layout';
-import { SEO } from 'components/seo';
-import ArtDirectorateSection from 'components/team-page/art-directorate/section/art-directorate-section';
-import FestivalTeamSection from 'components/team-page/festival-team/festival-team-section';
-import { fetcher } from 'services/fetcher';
+// import { AboutUsLayout } from 'components/about-us-layout';
+// import { AppLayout } from 'components/app-layout';
+// import { SEO } from 'components/seo';
+// import ArtDirectorateSection from 'components/team-page/art-directorate/section/art-directorate-section';
+// import FestivalTeamSection from 'components/team-page/festival-team/festival-team-section';
+// import { fetcher } from 'services/fetcher';
 
-import type { FestivalTeams } from '__generated__/api-typings';
-import type { InferGetServerSidePropsType } from 'next';
+// import type { FestivalTeams } from '__generated__/api-typings';
+// import type { InferGetServerSidePropsType } from 'next';
 
-const Team = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const {
-    artDirectorate,
-    restTeam,
-  } = props;
+// const Team = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+//   const {
+//     artDirectorate,
+//     restTeam,
+//   } = props;
 
-  return (
-    <AppLayout>
-      <SEO title="Организаторы"/>
-      <AboutUsLayout>
-        <ArtDirectorateSection cards={artDirectorate}/>
-        <FestivalTeamSection cards={restTeam}/>
-      </AboutUsLayout>
-    </AppLayout>
-  );
-};
+//   return (
+//     <AppLayout>
+//       <SEO title="Организаторы"/>
+//       <AboutUsLayout>
+//         <ArtDirectorateSection cards={artDirectorate}/>
+//         <FestivalTeamSection cards={restTeam}/>
+//       </AboutUsLayout>
+//     </AppLayout>
+//   );
+// };
 
-export const getServerSideProps = async () => {
-  const team = await fetcher<FestivalTeams[]>('/info/about-festival/team/');
+// export const getServerSideProps = async () => {
+//   const team = await fetcher<FestivalTeams[]>('/info/about-festival/team/');
 
-  const { art: artDirectorate, fest: restTeam } = team.reduce<Record<string, FestivalTeams[]>>((acc, entry) => {
-    (acc[entry.team] || (acc[entry.team] = [])).push(entry);
+//   const { art: artDirectorate, fest: restTeam } = team.reduce<Record<string, FestivalTeams[]>>((acc, entry) => {
+//     (acc[entry.team] || (acc[entry.team] = [])).push(entry);
 
-    return acc;
-  }, {});
+//     return acc;
+//   }, {});
 
-  return {
-    props: {
-      artDirectorate,
-      restTeam,
-    },
-  };
+//   return {
+//     props: {
+//       artDirectorate,
+//       restTeam,
+//     },
+//   };
+// };
+
+// export default Team;
+
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+const Team = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/about-us/404');
+  }, []);
+
+  return null;
 };
 
 export default Team;
+
