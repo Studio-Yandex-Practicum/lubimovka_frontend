aed35baf4e1fd706e02f3383f171acec5732e2bf
Aleksey <1862647@gmail.com>
2023-09-16 12:00:47 +0700

    fix: ошибка при валидации email

--- src/shared/constants/regexps.ts
+++ src/shared/constants/regexps.ts
@@ -1,3 +1,3 @@
 export const validYearRegexp = /^(19|20)[0-9]{2}$/;
 export const validPhoneNumberRegexp = /[\d]{2}$/;
-export const validEmailRegexp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+export const validEmailRegexp = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
