683e234eacd7eabcccb443e46d12f0312fc80989
Alexander Konashin <konashin.alexander@yandex.ru>
2023-09-28 15:03:08 +0300

    fix: исправлен модуль play-list

--- src/components/play-list/play-list.tsx
+++ src/components/play-list/play-list.tsx
@@ -5,20 +5,21 @@ import style from './play-list.module.css';
 const cx = classNames.bind(style);
 
 interface PlayListProps {
-  variant?: 'regular' | 'scrollable'
-  processing?: boolean
+  variant: 'regular' | 'scrollable'
+  className?: boolean
 }
 
 export const PlayList = (props: React.PropsWithChildren<PlayListProps>) => {
   const {
     variant = 'regular',
-    processing,
+    className,
     children,
   } = props;
 
   return (
-    <ul className={cx(variant, { processing })}>
+    <ul className={cx(variant, className)}>
       {children}
     </ul>
   );
 };
+
