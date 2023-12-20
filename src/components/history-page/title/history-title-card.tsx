4421588ff18966bd09d3a992c7405b7b6f8cda44
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-12-20 13:34:46 +0400

    Исправлен показ статистики, исправлена логика ссылок, оптимизирован компонент

---
+++ src/components/history-page/title/history-title-card.tsx
@@ -0,0 +1,35 @@
+import cn from 'classnames';
+import { ReactElement } from 'react';
+
+import { Button } from 'components/ui/button2';
+import { IIconProps } from 'components/ui/icon';
+
+import style from './history-title.module.css';
+
+export interface ICard {
+  title: string
+  count?: number
+  buttonProps?: {
+    [p:string]: string | ReactElement<IIconProps>
+  }
+}
+
+export const HistoryTitleCard = ({ item }: {item: ICard}) => {
+  return (
+    <div className={cn(style.card)}>
+      <div className={style.buttonDisplay}>
+        <Button
+          type='button'
+          size='xxl'
+          border='none'
+          {...(item.count && item.buttonProps)}
+        >
+          {item.count ? item.count.toString() : '0'}
+        </Button>
+      </div>
+      <p className={cn(style.element)}>
+        {item.title}
+      </p>
+    </div>
+  );
+};
