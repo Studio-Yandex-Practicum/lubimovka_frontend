137b4fd3f4ccf7a30532fbd6f55cb9d6af1d8a85
VLGrigoryan <120823432+VLGrigoryan@users.noreply.github.com>
2024-08-30 01:22:16 +0300

    Update history-title.tsx

--- src/components/history-page/title/history-title.tsx
+++ src/components/history-page/title/history-title.tsx
@@ -69,7 +69,7 @@ export const HistoryTitle: React.FC<IHistoryTitle> = ({ data, currentYear, showV
           width="100%"
           height="100%"
         />
-      } : null
+      } : undefined
     },
     events_count: {
       count: events_count,
