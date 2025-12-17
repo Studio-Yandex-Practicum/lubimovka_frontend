cd8ebbabe30d84f1ff41073e05bc076fc6bec94d
Rodion Kostyuchenko <rodion.kostyuchenko@gmail.com>
2025-12-17 10:54:35 +0300

    Обновление msw и перегенерация lock-файла

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,9 +1,9 @@
 export async function setupMocks() {
-  if (typeof window !== 'undefined') {
-    const { worker } = await import('mocks/browser');
-    worker.start();
-  } else {
+  if (typeof window == 'undefined') {
     const { server } = await import ('mocks/server');
     server.listen();
+  } else {
+    const { worker } = await import('mocks/browser');
+    worker.start();
   }
 }
